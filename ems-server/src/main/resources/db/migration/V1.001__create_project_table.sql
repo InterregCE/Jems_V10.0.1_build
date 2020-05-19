020cbd1ce9482b03ea73846a48c2636ba6811346
Vlad Teodorescu <Vlad.Teodorescu@cloudflight.io>
2020-05-19 13:05:56 +0300

    Add db setup to the application.yaml with env variables, flyway migrations and default values.

--- ems-server/src/main/resources/db/migration/V1.001__create_project_table.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE project (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(128) NOT NULL
-);
-
-INSERT INTO project (name) values ('First Application');
---
+++ ems-server/src/main/resources/db/migration/V202005191242__create_project_table.sql
@@ -0,0 +1,5 @@
+CREATE TABLE project (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    acronym VARCHAR(25) NOT NULL,
+    submission_date DATE NOT NULL
+);
