73c3088ad2d67c48ca64b45d6981f3313de369d8
Vlad Teodorescu <Vlad.Teodorescu@cloudflight.io>
2020-06-17 08:40:53 +0300

    MP2-251 Add error message and validation for 100 characters. Add truncation at 30 characters and the entire message on hover.

--- ems-ui/src/app/components/general/cell-renderers/description-cell/description-cell.component.ts
+++ ems-ui/src/app/components/general/cell-renderers/description-cell/description-cell.component.ts
@@ -1,4 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
+import {FormBuilder, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-description-cell',
@@ -9,19 +10,45 @@ export class DescriptionCellComponent implements OnInit {
   @Input()
   data: any;
 
-  description: string;
+  descriptionForm = this.formBuilder.group({
+    descriptionArea: ['', Validators.compose([
+      Validators.maxLength(100)
+    ])]
+  });
 
-  ngOnInit(): void {
-    this.description = this.data.row.description;
+  constructor(private formBuilder: FormBuilder) {
   }
 
-  onSubmit(): void {
-    this.data.extraProps.onSave(this.description, this.data.index, this.data.row.id);
+  get description() {
+    return this.descriptionForm.controls.descriptionArea;
+  }
+
+  ngOnInit(): void {
+    this.descriptionForm.patchValue({descriptionArea: this.data.row.description});
+  }
+
+  onSubmit(): any {
+    if (this.description.value.length <= 100) {
+      this.data.extraProps.onSave(this.description.value, this.data.index, this.data.row.id);
+    }
   }
 
   cancelSave(): void {
-    this.description = this.data.row.description;
+    this.descriptionForm.patchValue({descriptionArea: this.data.row.description});
     this.data.extraProps.onCancel(this.data.index);
   }
 
+  getDescriptionError(): string {
+    if (this.description.value.length > 100) {
+      return 'project.file.description.size.too.long';
+    }
+    return '';
+  }
+
+  getSizedDescription(): string {
+    if (this.data.row.description.length > 30) {
+      return this.data.row.description.substring(0, 30) + '...';
+    }
+    return this.data.row.description;
+  }
 }
