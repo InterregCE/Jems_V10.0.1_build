d7d079922b646df93ee964bc0059182a13f1d524
Ondrej Oravčok <Ondrej.Oravcok@cloudflight.io>
2020-06-19 13:40:38 +0200

    MP2-51 created dto with password, so entity not needed to go out of service

--- ems-server/src/main/kotlin/io/cloudflight/ems/service/UserService.kt
+++ ems-server/src/main/kotlin/io/cloudflight/ems/service/UserService.kt
@@ -1,12 +1,12 @@
 package io.cloudflight.ems.service
 
 import io.cloudflight.ems.api.dto.OutputUser
-import io.cloudflight.ems.entity.Account
+import io.cloudflight.ems.dto.UserWithCredentials
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 
 interface UserService {
-    fun findOneByEmail(email: String): Account?
+    fun findOneByEmail(email: String): UserWithCredentials?
     fun getByEmail(email: String): OutputUser?
     fun findAll(pageable: Pageable): Page<OutputUser>
 }
