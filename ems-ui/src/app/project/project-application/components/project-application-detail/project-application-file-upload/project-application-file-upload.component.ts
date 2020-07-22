d4ffdd143fa4a930426eba7525c4fda5f56abc02
Mihai Rinzis <mihai.rinzis@cloudflight.io>
2020-07-22 12:45:04 +0300

    Chage file upload messages to alerts

--- ems-ui/src/app/project/project-application/components/project-application-detail/project-application-file-upload/project-application-file-upload.component.ts
+++ ems-ui/src/app/project/project-application/components/project-application-detail/project-application-file-upload/project-application-file-upload.component.ts
@@ -1,6 +1,9 @@
-import {ChangeDetectionStrategy, Component, EventEmitter, Input, Output} from '@angular/core';
+import {ChangeDetectionStrategy, ChangeDetectorRef, Component, EventEmitter, Input, Output} from '@angular/core';
 import {OutputProjectStatus} from '@cat/api'
 import {Permission} from '../../../../../security/permissions/permission';
+import {Alert} from '@common/components/forms/alert';
+import {AbstractForm} from '@common/components/forms/abstract-form';
+import {FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-project-application-file-upload',
@@ -8,14 +11,13 @@ import {Permission} from '../../../../../security/permissions/permission';
   styleUrls: ['./project-application-file-upload.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ProjectApplicationFileUploadComponent {
+export class ProjectApplicationFileUploadComponent extends AbstractForm {
   OutputProjectStatus = OutputProjectStatus;
   Permission = Permission;
+  Alert = Alert;
 
   @Input()
   fileNumber: number;
-  @Input()
-  statusMessages: string[];
   @Input()
   projectStatus: OutputProjectStatus.StatusEnum;
   @Input()
@@ -24,7 +26,18 @@ export class ProjectApplicationFileUploadComponent {
   @Output()
   uploadFile: EventEmitter<File> = new EventEmitter<File>();
 
+  uploadedFileName: string;
+
+  constructor(protected changeDetectorRef: ChangeDetectorRef) {
+    super(changeDetectorRef);
+  }
+
   addNewFilesForUpload($event: any): void {
+    this.uploadedFileName = $event?.target?.files[0]?.name
     this.uploadFile.emit($event.target.files[0]);
+  }
+
+  getForm(): FormGroup | null {
+    return null;
   }
 }
