f423e1dda167f349f8f361420e0432742531712c
Raphael Seywerth <raphael.seywerth@catalysts.cc>
2020-08-10 20:20:19 +0200

    MP2-496 rebased, added javadoc for end time mapping treatment (incl. specified minute)

--- ems-server/src/main/kotlin/io/cloudflight/ems/call/service/CallMapper.kt
+++ ems-server/src/main/kotlin/io/cloudflight/ems/call/service/CallMapper.kt
@@ -9,6 +9,10 @@ import io.cloudflight.ems.entity.User
 import io.cloudflight.ems.programme.entity.ProgrammePriorityPolicy
 import io.cloudflight.ems.programme.service.toOutputProgrammePriorityPolicy
 
+/**
+ * Map InputCallCreate to entity Call.
+ * The specified minute is included in the end time of the call by adding all of its seconds.
+ */
 fun InputCallCreate.toEntity(creator: User, priorityPolicies: Set<ProgrammePriorityPolicy>) = Call(
     id = null,
     creator = creator,
