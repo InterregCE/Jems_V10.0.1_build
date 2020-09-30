de9281fad7a6c5ce9698c79acc13758903caf7bc
Mihai Rinzis <mihai.rinzis@cloudflight.io>
2020-09-30 09:15:41 +0300

    MP-570 fix top bar service tests

--- ems-ui/src/app/common/components/top-bar/top-bar-service.spec.ts
+++ ems-ui/src/app/common/components/top-bar/top-bar-service.spec.ts
@@ -1,14 +1,11 @@
 import {fakeAsync, TestBed, tick} from '@angular/core/testing';
 import {TopBarService} from '@common/components/top-bar/top-bar.service';
 import {TestModule} from '../../test-module';
-import {PermissionService} from '../../../security/permissions/permission.service';
 import {MenuItemConfiguration} from '@common/components/menu/model/menu-item.configuration';
 import {Permission} from '../../../security/permissions/permission';
 import {SecurityService} from '../../../security/security.service';
-import {HttpTestingController} from '@angular/common/http/testing';
 
 describe('TopBarService', () => {
-  let httpTestingController: HttpTestingController;
   let service: TopBarService;
 
   beforeEach(() => {
@@ -21,7 +18,6 @@ describe('TopBarService', () => {
         },
       ]
     });
-    httpTestingController = TestBed.inject(HttpTestingController);
     service = TestBed.inject(TopBarService);
   });
 
@@ -30,41 +26,34 @@ describe('TopBarService', () => {
   });
 
   it('should create menus depending on rights', fakeAsync(() => {
-    const permissionService: PermissionService = TestBed.inject(PermissionService);
     const securityService: SecurityService = TestBed.inject(SecurityService);
-    securityService.reloadCurrentUser().subscribe();
     service.newAuditUrl('auditUrl');
-
-    // Error: Expected one matching request for criteria "Match method: GET, URL: //api/auth/current", found 2 requests.
-    httpTestingController.expectOne({
-      method: 'GET',
-      url: `//api/auth/current`
-    }).flush({id: 1, name: 'user', role: 'role'});
 
     let menuItems: MenuItemConfiguration[] = [];
     service.menuItems()
       .subscribe((items: MenuItemConfiguration[]) => menuItems = items);
 
-    permissionService.setPermissions([Permission.ADMINISTRATOR]);
+    (securityService as any).myCurrentUser.next({name: 'user', role: Permission.ADMINISTRATOR});
     tick();
     expect(menuItems.length).toBe(6);
-    expect(menuItems[0].name).toBe('topbar.main.project')
-    expect(menuItems[1].name).toBe('topbar.main.call')
-    expect(menuItems[2].name).toBe('topbar.main.programme')
-    expect(menuItems[3].name).toBe('topbar.main.user.management')
-    expect(menuItems[4].name).toBe('topbar.main.audit')
-    expect(menuItems[5].name).toBe('user (role)')
+    expect(menuItems[0].name).toBe('topbar.main.project');
+    expect(menuItems[1].name).toBe('topbar.main.call');
+    expect(menuItems[2].name).toBe('topbar.main.programme');
+    expect(menuItems[3].name).toBe('topbar.main.user.management');
+    expect(menuItems[4].name).toBe('topbar.main.audit');
+    expect(menuItems[5].name).toBe('user (administrator)');
 
-    permissionService.setPermissions([Permission.PROGRAMME_USER]);
+    (securityService as any).myCurrentUser.next({name: 'user', role: Permission.PROGRAMME_USER});
     tick();
     expect(menuItems.length).toBe(5);
-    expect(menuItems[0].name).toBe('topbar.main.project')
-    expect(menuItems[1].name).toBe('topbar.main.call')
-    expect(menuItems[2].name).toBe('topbar.main.programme')
-    expect(menuItems[3].name).toBe('topbar.main.audit')
-    expect(menuItems[4].name).toBe('user (role)')
+    expect(menuItems[0].name).toBe('topbar.main.project');
+    expect(menuItems[1].name).toBe('topbar.main.call');
+    expect(menuItems[2].name).toBe('topbar.main.programme');
+    expect(menuItems[3].name).toBe('topbar.main.audit');
+    expect(menuItems[4].name).toBe('user (programme user)');
 
-    permissionService.setPermissions([Permission.APPLICANT_USER]);
+
+    (securityService as any).myCurrentUser.next({name: 'user', role: Permission.APPLICANT_USER});
     tick();
     expect(menuItems.length).toBe(3);
   }));
