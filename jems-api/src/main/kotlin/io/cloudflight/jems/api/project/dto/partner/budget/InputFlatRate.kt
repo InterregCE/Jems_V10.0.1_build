02f4871d34dc599a9deaa8dbd072b5823f978f8a
Ondrej Orav?ok <Ondrej.Oravcok@cloudflight.io>
2020-10-16 11:29:16 +0200

    MP2-729 add partner budget - office and administration (flatrate)

---
+++ jems-api/src/main/kotlin/io/cloudflight/jems/api/project/dto/partner/budget/InputFlatRate.kt
@@ -0,0 +1,10 @@
+package io.cloudflight.jems.api.project.dto.partner.budget
+
+import javax.validation.constraints.Max
+import javax.validation.constraints.Min
+
+data class InputFlatRate(
+    @field:Max(15)
+    @field:Min(0)
+    val value: Int?
+)
--- jems-server/src/main/kotlin/io/cloudflight/jems/server/project/service/partner/budget/ProjectPartnerBudgetServiceImpl.kt
+++ jems-server/src/main/kotlin/io/cloudflight/jems/server/project/service/partner/budget/ProjectPartnerBudgetServiceImpl.kt
@@ -4,10 +4,12 @@ import io.cloudflight.jems.api.project.dto.partner.budget.InputBudget
 import io.cloudflight.jems.server.exception.I18nValidationException
 import io.cloudflight.jems.server.exception.ResourceNotFoundException
 import io.cloudflight.jems.server.project.entity.partner.budget.CommonBudget
+import io.cloudflight.jems.server.project.entity.partner.budget.ProjectPartnerBudgetOfficeAdministration
 import io.cloudflight.jems.server.project.repository.partner.ProjectPartnerRepository
 import io.cloudflight.jems.server.project.repository.partner.budget.ProjectPartnerBudgetEquipmentRepository
 import io.cloudflight.jems.server.project.repository.partner.budget.ProjectPartnerBudgetExternalRepository
 import io.cloudflight.jems.server.project.repository.partner.budget.ProjectPartnerBudgetInfrastructureRepository
+import io.cloudflight.jems.server.project.repository.partner.budget.ProjectPartnerBudgetOfficeAdministrationRepository
 import io.cloudflight.jems.server.project.repository.partner.budget.ProjectPartnerBudgetStaffCostRepository
 import io.cloudflight.jems.server.project.repository.partner.budget.ProjectPartnerBudgetTravelRepository
 import org.springframework.http.HttpStatus
@@ -23,7 +25,8 @@ class ProjectPartnerBudgetServiceImpl(
         private val projectPartnerBudgetTravelRepository: ProjectPartnerBudgetTravelRepository,
         private val projectPartnerBudgetExternalRepository: ProjectPartnerBudgetExternalRepository,
         private val projectPartnerBudgetEquipmentRepository: ProjectPartnerBudgetEquipmentRepository,
-        private val projectPartnerBudgetInfrastructureRepository: ProjectPartnerBudgetInfrastructureRepository
+        private val projectPartnerBudgetInfrastructureRepository: ProjectPartnerBudgetInfrastructureRepository,
+        private val projectPartnerBudgetOfficeAdministrationRepository: ProjectPartnerBudgetOfficeAdministrationRepository
 ) : ProjectPartnerBudgetService {
 
     companion object {
@@ -165,6 +168,29 @@ class ProjectPartnerBudgetServiceImpl(
             .map { it.toOutput() }
     }
     //endregion Infrastructure
+
+    @Transactional(readOnly = true)
+    override fun getOfficeAdministrationFlatRate(projectId: Long, partnerId: Long): Int? {
+        if (projectPartnerRepository.findFirstByProjectIdAndId(projectId, partnerId).isEmpty)
+            throw ResourceNotFoundException("projectPartner")
+
+        return projectPartnerBudgetOfficeAdministrationRepository.findById(partnerId)
+            .map { it?.flatRate }.orElse(null)
+    }
+
+    @Transactional
+    override fun updateOfficeAdministrationFlatRate(projectId: Long, partnerId: Long, flatRate: Int?): Int? {
+        if (projectPartnerRepository.findFirstByProjectIdAndId(projectId, partnerId).isEmpty)
+            throw ResourceNotFoundException("projectPartner")
+
+        if (flatRate != null)
+            return projectPartnerBudgetOfficeAdministrationRepository.save(
+                ProjectPartnerBudgetOfficeAdministration(partnerId, flatRate)
+            ).flatRate
+
+        projectPartnerBudgetOfficeAdministrationRepository.deleteById(partnerId)
+        return null
+    }
 
     private fun validateInput(projectId: Long, partnerId: Long, budgetList: List<InputBudget> = emptyList()) {
         if (projectPartnerRepository.findFirstByProjectIdAndId(projectId, partnerId).isEmpty)
