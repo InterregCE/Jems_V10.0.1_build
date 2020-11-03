eba6e7e71f15d24126ea5e4cced8472c29ff073e
Mihai Rinzis <mihai@rmi.cat>
2020-11-03 15:37:46 +0200

    MP2-719 make the sidebar headlines expandable on link click

--- jems-ui/src/app/common/components/side-nav/component/side-nav.component.ts
+++ jems-ui/src/app/common/components/side-nav/component/side-nav.component.ts
@@ -1,6 +1,7 @@
-import {ChangeDetectionStrategy, Component, Input} from '@angular/core';
+import {ChangeDetectionStrategy, Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 import {SideNavService} from '@common/components/side-nav/side-nav.service';
 import {HeadlineRoute} from '@common/components/side-nav/headline-route';
+import {SelectionModel} from '@angular/cdk/collections';
 
 @Component({
   selector: 'app-side-nav',
@@ -8,15 +9,39 @@ import {HeadlineRoute} from '@common/components/side-nav/headline-route';
   styleUrls: ['./side-nav.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class SideNavComponent {
+export class SideNavComponent implements OnInit, OnChanges {
 
   @Input()
   headlines: HeadlineRoute[];
 
-  constructor( public sideNavService: SideNavService){}
+  expanded = new SelectionModel<HeadlineRoute>(true);
 
-  getExpansionDisabledStatus(bullets: HeadlineRoute[]): boolean{
-    return !bullets || bullets.length === 0;
+  constructor(public sideNavService: SideNavService) {
+  }
+
+  ngOnInit(): void {
+    this.setExpanded()
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes.headlines) {
+      this.setExpanded();
+    }
+  }
+
+  setExpanded(): void {
+    this.expanded.clear();
+    const toExpand: HeadlineRoute[] = [];
+    this.headlines.forEach(root => this.geExpandableHeadlines(root, toExpand));
+    this.expanded.select(...toExpand);
+  }
+
+  private geExpandableHeadlines(headline: HeadlineRoute, expandable: HeadlineRoute[]): void {
+    if (!headline?.bullets?.length) {
+      return;
+    }
+    expandable.push(headline);
+    headline.bullets.forEach(entry => this.geExpandableHeadlines(entry, expandable));
   }
 
 }
