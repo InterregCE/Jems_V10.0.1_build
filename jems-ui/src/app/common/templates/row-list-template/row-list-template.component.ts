d24a37f2de6986936a1d55a5c9223e5aabab35ea
Mehran Khaksar haghani dehkordi <mehran.haghani@cloudflight.io>
2020-11-05 08:15:34 +0000

    MP2-668 - fixes layout problems in c.7 section

---
+++ jems-ui/src/app/common/templates/row-list-template/row-list-template.component.ts
@@ -0,0 +1,27 @@
+import {AfterViewInit, ChangeDetectionStrategy, Component, ElementRef, Input, ViewChild} from '@angular/core';
+
+@Component({
+  selector: 'app-row-list-template',
+  templateUrl: './row-list-template.component.html',
+  styleUrls: ['./row-list-template.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class RowListTemplateComponent implements AfterViewInit {
+
+  @ViewChild('container') container: ElementRef
+  @Input() columnClassList: string[]
+
+  ngAfterViewInit(): void {
+    if (!this.container.nativeElement) return;
+
+    const rows = this.container.nativeElement.children;
+    for (const row of rows) {
+      const columns = row.children;
+      for (let i = 0; i < columns.length; ++i) {
+        if (this.columnClassList[i] && this.columnClassList[i].length > 0) {
+          columns[i].classList.add(this.columnClassList[i])
+        }
+      }
+    }
+  }
+}
