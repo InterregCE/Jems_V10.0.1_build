1c120a3a7767ebb2e76cee93813467eece87e7f2
Mehran Khaksar haghani dehkordi <Mehran.Khaksarhaghanidehkordi@cloudflight.io>
2021-01-07 13:00:23 +0100

    MP2-919 - rebase onto master

--- jems-ui/src/app/project/project.module.ts
+++ jems-ui/src/app/project/project.module.ts
@@ -80,11 +80,8 @@ import {StaffCostsBudgetTableComponent} from './partner/project-partner-detail-p
 import {GeneralBudgetTableComponent} from './partner/project-partner-detail-page/project-partner-budget-tab/project-partner-budget/general-budget-table/general-budget-table.component';
 import {GeneralBudgetFlatRateTableComponent} from './partner/project-partner-detail-page/project-partner-budget-tab/project-partner-budget/general-budget-flat-rate-table/general-budget-flat-rate-table.component';
 import {ProjectWorkPackageActivitiesTabComponent} from './work-package/work-package-detail-page/project-work-package-activities-tab/project-work-package-activities-tab.component';
-import { TravelAndAccommodationCostsBudgetTableComponent } from './partner/project-partner-detail-page/project-partner-budget-tab/project-partner-budget/travel-and-accommodation-costs-budget-table/travel-and-accommodation-costs-budget-table.component';
+import {TravelAndAccommodationCostsBudgetTableComponent} from './partner/project-partner-detail-page/project-partner-budget-tab/project-partner-budget/travel-and-accommodation-costs-budget-table/travel-and-accommodation-costs-budget-table.component';
 import {ProjectPeriodsSelectComponent} from './components/project-periods-select/project-periods-select.component';
-import {StaffCostTableComponent} from './partner/project-partner-detail-page/project-partner-budget-tab/project-partner-budget/staff-cost-table/staff-cost-table.component';
-import {BudgetTableComponent} from './partner/project-partner-detail-page/project-partner-budget-tab/project-partner-budget/budget-table/budget-table.component';
-import {BudgetFlatRateTableComponent} from './partner/project-partner-detail-page/project-partner-budget-tab/project-partner-budget/budget-flat-rate-table/budget-flat-rate-table.component';
 import {ProjectWorkPackageInvestmentsTabComponent} from './work-package/work-package-detail-page/project-work-package-investments-tab/project-work-package-investments-tab.component';
 import {ProjectWorkPackageInvestmentDetailPageComponent} from './work-package/work-package-detail-page/project-work-package-investments-tab/project-work-package-investment-detail-page/project-work-package-investment-detail-page.component';
 import {WorkPackageInvestmentDeleteActionCellComponent} from './work-package/work-package-detail-page/project-work-package-investments-tab/work-package-investment-delete-action-cell/work-package-investment-delete-action-cell.component';
@@ -169,8 +166,6 @@ import {WorkPackageInvestmentDeleteActionCellComponent} from './work-package/wor
     ProjectWorkPackageInvestmentsTabComponent,
     ProjectWorkPackageInvestmentDetailPageComponent,
     WorkPackageInvestmentDeleteActionCellComponent,
-    StaffCostTableComponent,
-    BudgetFlatRateTableComponent,
     ProjectPeriodsSelectComponent
   ],
   imports: [
