adb3a0423b4603eb857349e01c3d3cc165c022de
Mihai Rinzis <mihai@rmi.cat>
2021-03-05 16:28:19 +0200

    MP2-1104 rework project overview

--- jems-ui/src/app/common/utils/tables.ts
+++ jems-ui/src/app/common/utils/tables.ts
@@ -5,7 +5,7 @@ export class Tables {
   static DEFAULT_INITIAL_PAGE_INDEX = 0;
   static DEFAULT_INITIAL_PAGE_SIZE = 25;
   static DEFAULT_INITIAL_SORT: Partial<MatSort> = {active: 'id', direction: 'desc'};
-  static DEFAULT_DATE_FORMAT = 'dd.MM.yyyy HH:mm';
+  static DEFAULT_DATE_FORMAT = 'DD.MM.YYYY HH:mm';
   static DEFAULT_DATE_FORMAT_NO_TIME = 'dd.MM.yyyy';
 
   static getNextId(entries: any[]): number {
