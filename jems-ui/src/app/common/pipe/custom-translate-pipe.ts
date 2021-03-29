79c0e7fed535b2c6b1511afdd7b59d6591f90a47
Mehran Khaksar haghani dehkordi <Mehran.Khaksarhaghanidehkordi@cloudflight.io>
2021-03-29 08:24:25 +0200

    MP2-1380 - adds fallback language support for translate pipe

---
+++ jems-ui/src/app/common/pipe/custom-translate-pipe.ts
@@ -0,0 +1,32 @@
+import {TranslatePipe, TranslateService} from '@ngx-translate/core';
+import {ChangeDetectorRef, Pipe, PipeTransform} from '@angular/core';
+import {LanguageStore} from '../services/language-store.service';
+
+// This pipe will replace the 'translate' pipe from '@ngx-translate'
+// It's extended the TranslatePipe to provide support for using the fallback language's translation
+// For translation keys that exist in the translation file but their translation is empty
+// Note: for translation keys that don't exist in the translation file the setDefaultLang of TranslateService will do the same
+@Pipe({name: 'translate', pure: false})
+export class CustomTranslatePipe extends TranslatePipe implements PipeTransform {
+
+  translateService: TranslateService;
+
+  constructor(private languageStore: LanguageStore, translate: TranslateService, _ref: ChangeDetectorRef) {
+    super(translate, _ref);
+    this.translateService = translate;
+  }
+
+  transform(query: string, ...args: any[]): any {
+    if (!(query && query.length > 0)) {
+      return '';
+    }
+    let translation = super.transform(query, ...args);
+    if (translation === null || translation === '') {
+      const currentLang = this.translateService.currentLang;
+      this.translateService.use(this.languageStore.getFallbackLanguageValue());
+      translation = this.translateService.instant(query, args);
+      this.translateService.use(currentLang);
+    }
+    return translation;
+  }
+}
