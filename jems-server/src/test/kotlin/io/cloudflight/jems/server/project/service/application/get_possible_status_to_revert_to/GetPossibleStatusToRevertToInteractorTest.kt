f954df34b9663aa81bd1aaf777e1147c72d5f280
Ondrej Oravčok <Ondrej.Oravcok@cloudflight.io>
2021-04-12 13:56:11 +0200

    MP2-1408 added unit tests for application status service

---
+++ jems-server/src/test/kotlin/io/cloudflight/jems/server/project/service/application/get_possible_status_to_revert_to/GetPossibleStatusToRevertToInteractorTest.kt
@@ -0,0 +1,53 @@
+package io.cloudflight.jems.server.project.service.application.get_possible_status_to_revert_to
+
+import io.cloudflight.jems.server.UnitTest
+import io.cloudflight.jems.server.project.service.ProjectPersistence
+import io.cloudflight.jems.server.project.service.application.ApplicationStatus
+import io.cloudflight.jems.server.project.service.application.workflow.ApplicationStateFactory
+import io.cloudflight.jems.server.project.service.application.workflow.states.NotApprovedApplicationState
+import io.cloudflight.jems.server.project.service.model.ProjectSummary
+import io.mockk.every
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.impl.annotations.MockK
+import io.mockk.impl.annotations.RelaxedMockK
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.springframework.context.ApplicationEventPublisher
+
+class GetPossibleStatusToRevertToInteractorTest : UnitTest() {
+
+    companion object {
+        private const val PROJECT_ID = 1L
+        private val summary = ProjectSummary(
+            id = PROJECT_ID,
+            acronym = "project acronym",
+            status = ApplicationStatus.NOT_APPROVED
+        )
+    }
+
+    @MockK
+    lateinit var projectPersistence: ProjectPersistence
+
+    @MockK
+    lateinit var applicationStateFactory: ApplicationStateFactory
+
+    @RelaxedMockK
+    lateinit var auditPublisher: ApplicationEventPublisher
+
+    @InjectMockKs
+    private lateinit var getPossibleStatusToRevertTo: GetPossibleStatusToRevertTo
+
+    @MockK
+    lateinit var notApprovedState: NotApprovedApplicationState
+
+
+    @Test
+    fun get() {
+        every { projectPersistence.getProjectSummary(PROJECT_ID) } returns summary
+        every { applicationStateFactory.getInstance(any()) } returns notApprovedState
+        every { notApprovedState.getPossibleStatusToRevertTo() } returns ApplicationStatus.ELIGIBLE
+
+        assertThat(getPossibleStatusToRevertTo.get(PROJECT_ID)).isEqualTo(ApplicationStatus.ELIGIBLE)
+    }
+
+}
---
+++ jems-server/src/test/kotlin/io/cloudflight/jems/server/project/service/application/return_application_to_applicant/ReturnApplicationToApplicantInteractorTest.kt
@@ -0,0 +1,68 @@
+package io.cloudflight.jems.server.project.service.application.return_application_to_applicant
+
+import io.cloudflight.jems.api.audit.dto.AuditAction
+import io.cloudflight.jems.server.UnitTest
+import io.cloudflight.jems.server.audit.model.AuditProject
+import io.cloudflight.jems.server.audit.service.AuditCandidate
+import io.cloudflight.jems.server.project.service.ProjectPersistence
+import io.cloudflight.jems.server.project.service.application.ApplicationStatus
+import io.cloudflight.jems.server.project.service.application.workflow.ApplicationStateFactory
+import io.cloudflight.jems.server.project.service.application.workflow.states.SubmittedApplicationState
+import io.cloudflight.jems.server.project.service.model.ProjectSummary
+import io.mockk.every
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.impl.annotations.MockK
+import io.mockk.impl.annotations.RelaxedMockK
+import io.mockk.slot
+import io.mockk.verify
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.springframework.context.ApplicationEventPublisher
+
+class ReturnApplicationToApplicantInteractorTest : UnitTest() {
+
+    companion object {
+        private const val PROJECT_ID = 1L
+        private val summary = ProjectSummary(
+            id = PROJECT_ID,
+            acronym = "project acronym",
+            status = ApplicationStatus.SUBMITTED
+        )
+    }
+
+    @MockK
+    lateinit var projectPersistence: ProjectPersistence
+
+    @MockK
+    lateinit var applicationStateFactory: ApplicationStateFactory
+
+    @RelaxedMockK
+    lateinit var auditPublisher: ApplicationEventPublisher
+
+    @InjectMockKs
+    private lateinit var returnApplicationToApplicant: ReturnApplicationToApplicant
+
+    @MockK
+    lateinit var submittedState: SubmittedApplicationState
+
+
+    @Test
+    fun returnToApplicant() {
+        every { projectPersistence.getProjectSummary(PROJECT_ID) } returns summary
+        every { applicationStateFactory.getInstance(any()) } returns submittedState
+        every { submittedState.returnToApplicant() } returns ApplicationStatus.RETURNED_TO_APPLICANT
+
+        assertThat(returnApplicationToApplicant.returnToApplicant(PROJECT_ID)).isEqualTo(ApplicationStatus.RETURNED_TO_APPLICANT)
+
+        val slotAudit = slot<AuditCandidate>()
+        verify(exactly = 1) { auditPublisher.publishEvent(capture(slotAudit)) }
+        assertThat(slotAudit.captured).isEqualTo(
+            AuditCandidate(
+                action = AuditAction.APPLICATION_STATUS_CHANGED,
+                project = AuditProject(id = PROJECT_ID.toString(), name = "project acronym"),
+                description = "Project application status changed from SUBMITTED to RETURNED_TO_APPLICANT"
+            )
+        )
+    }
+
+}
