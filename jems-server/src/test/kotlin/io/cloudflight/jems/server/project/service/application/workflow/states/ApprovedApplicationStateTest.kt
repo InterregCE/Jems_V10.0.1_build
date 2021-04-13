15fe11bcd69a61c1ec9f4bbb81bb1f75e6635cfa
Ondrej Orav?ok <Ondrej.Oravcok@cloudflight.io>
2021-04-13 15:56:42 +0200

    MP2-1408 add more unit tests to application status changes

---
+++ jems-server/src/test/kotlin/io/cloudflight/jems/server/project/service/application/workflow/states/ApprovedApplicationStateTest.kt
@@ -0,0 +1,148 @@
+package io.cloudflight.jems.server.project.service.application.workflow.states
+
+import io.cloudflight.jems.server.audit.service.AuditService
+import io.cloudflight.jems.server.authentication.service.SecurityService
+import io.cloudflight.jems.server.project.service.ProjectWorkflowPersistence
+import io.cloudflight.jems.server.project.service.application.ApplicationActionInfo
+import io.cloudflight.jems.server.project.service.application.ApplicationStatus
+import io.cloudflight.jems.server.project.service.application.workflow.ApproveIsNotAllowedException
+import io.cloudflight.jems.server.project.service.application.workflow.ApproveWithConditionsIsNotAllowedException
+import io.cloudflight.jems.server.project.service.application.workflow.DecisionReversionIsNotPossibleException
+import io.cloudflight.jems.server.project.service.application.workflow.RefuseIsNotAllowedException
+import io.cloudflight.jems.server.project.service.application.workflow.SetAsEligibleIsNotAllowedException
+import io.cloudflight.jems.server.project.service.application.workflow.SetAsIneligibleIsNotAllowedException
+import io.cloudflight.jems.server.project.service.application.workflow.SubmitIsNotAllowedException
+import io.cloudflight.jems.server.project.service.model.ProjectSummary
+import io.mockk.every
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.impl.annotations.MockK
+import io.mockk.impl.annotations.RelaxedMockK
+import io.mockk.junit5.MockKExtension
+import io.mockk.verify
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.api.extension.ExtendWith
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.EnumSource
+import java.time.LocalDate
+
+@ExtendWith(MockKExtension::class)
+class ApprovedApplicationStateTest {
+
+    companion object {
+        private const val PROJECT_ID = 8L
+        private const val USER_ID = 10L
+
+        private val actionInfo = ApplicationActionInfo(
+            note = "some dummy note",
+            date = LocalDate.now()
+        )
+    }
+
+    @MockK
+    lateinit var projectSummary: ProjectSummary
+
+    @MockK
+    lateinit var projectWorkflowPersistence: ProjectWorkflowPersistence
+
+    @RelaxedMockK
+    lateinit var auditService: AuditService
+
+    @MockK
+    lateinit var securityService: SecurityService
+
+    @InjectMockKs
+    private lateinit var approvedApplicationState: ApprovedApplicationState
+
+    @BeforeEach
+    fun setup() {
+        every { projectSummary.id } returns PROJECT_ID
+        every { projectSummary.status } returns ApplicationStatus.APPROVED
+        every { securityService.getUserIdOrThrow() } returns USER_ID
+    }
+
+    @Test
+    fun returnToApplicant() {
+        every { projectWorkflowPersistence.updateProjectCurrentStatus(any(), any(), any()) } returns ApplicationStatus.RETURNED_TO_APPLICANT
+
+        assertThat(approvedApplicationState.returnToApplicant()).isEqualTo(ApplicationStatus.RETURNED_TO_APPLICANT)
+        verify(exactly = 1) { projectWorkflowPersistence.updateProjectCurrentStatus(PROJECT_ID, USER_ID, ApplicationStatus.RETURNED_TO_APPLICANT)  }
+    }
+
+    @Test
+    fun `revertDecision to APPROVED_WITH_CONDITIONS`() {
+        val status = ApplicationStatus.APPROVED_WITH_CONDITIONS
+        every { projectWorkflowPersistence.getApplicationPreviousStatus(PROJECT_ID) } returns status
+        every { projectWorkflowPersistence.revertCurrentStatusToPreviousStatus(PROJECT_ID) } returns status
+        every { projectWorkflowPersistence.resetProjectFundingDecisionToCurrentStatus(PROJECT_ID) } returns status
+
+        assertThat(approvedApplicationState.revertDecision()).isEqualTo(status)
+        verify(exactly = 1) { projectWorkflowPersistence.revertCurrentStatusToPreviousStatus(PROJECT_ID)  }
+        verify(exactly = 1) { projectWorkflowPersistence.resetProjectFundingDecisionToCurrentStatus(PROJECT_ID)  }
+    }
+
+    @Test
+    fun `revertDecision to ELIGIBLE`() {
+        every { projectWorkflowPersistence.getApplicationPreviousStatus(PROJECT_ID) } returns ApplicationStatus.ELIGIBLE
+        every { projectWorkflowPersistence.revertCurrentStatusToPreviousStatus(PROJECT_ID) } returns ApplicationStatus.ELIGIBLE
+        every { projectWorkflowPersistence.clearProjectFundingDecision(PROJECT_ID) } answers { }
+
+        assertThat(approvedApplicationState.revertDecision()).isEqualTo(ApplicationStatus.ELIGIBLE)
+        verify(exactly = 1) { projectWorkflowPersistence.revertCurrentStatusToPreviousStatus(PROJECT_ID)  }
+        verify(exactly = 1) { projectWorkflowPersistence.clearProjectFundingDecision(PROJECT_ID)  }
+    }
+
+    @ParameterizedTest(name = "revertDecision to {0} - invalid")
+    @EnumSource(value = ApplicationStatus::class, names = ["ELIGIBLE", "APPROVED_WITH_CONDITIONS"], mode = EnumSource.Mode.EXCLUDE)
+    fun revertDecision(status: ApplicationStatus) {
+        every { projectWorkflowPersistence.getApplicationPreviousStatus(PROJECT_ID) } returns status
+        assertThrows<DecisionReversionIsNotPossibleException> { approvedApplicationState.revertDecision() }
+    }
+
+    @ParameterizedTest(name = "get possible status to revert to {0}")
+    @EnumSource(value = ApplicationStatus::class, names = ["ELIGIBLE", "APPROVED_WITH_CONDITIONS"])
+    fun getPossibleStatusToRevertTo(status: ApplicationStatus) {
+        every { projectWorkflowPersistence.getApplicationPreviousStatus(PROJECT_ID) } returns status
+        assertThat(approvedApplicationState.getPossibleStatusToRevertTo()).isEqualTo(status)
+    }
+
+    @ParameterizedTest(name = "getPossibleStatusToRevertTo {0} - invalid")
+    @EnumSource(value = ApplicationStatus::class, names = ["ELIGIBLE", "APPROVED_WITH_CONDITIONS"], mode = EnumSource.Mode.EXCLUDE)
+    fun `getPossibleStatusToRevertTo - invalid`(status: ApplicationStatus) {
+        every { projectWorkflowPersistence.getApplicationPreviousStatus(PROJECT_ID) } returns status
+        assertThat(approvedApplicationState.getPossibleStatusToRevertTo()).isNull()
+    }
+
+    @Test
+    fun submit() {
+        assertThrows<SubmitIsNotAllowedException> { approvedApplicationState.submit() }
+    }
+
+    @Test
+    fun setAsEligible() {
+        assertThrows<SetAsEligibleIsNotAllowedException> { approvedApplicationState.setAsEligible(actionInfo) }
+    }
+
+    @Test
+    fun setAsIneligible() {
+        assertThrows<SetAsIneligibleIsNotAllowedException> { approvedApplicationState.setAsIneligible(actionInfo) }
+    }
+
+    @Test
+    fun approve() {
+        assertThrows<ApproveIsNotAllowedException> { approvedApplicationState.approve(actionInfo) }
+    }
+
+    @Test
+    fun approveWithConditions() {
+        assertThrows<ApproveWithConditionsIsNotAllowedException> { approvedApplicationState.approveWithConditions(actionInfo) }
+    }
+
+    @Test
+    fun refuse() {
+        assertThrows<RefuseIsNotAllowedException> { approvedApplicationState.refuse(actionInfo) }
+    }
+
+}
---
+++ jems-server/src/test/kotlin/io/cloudflight/jems/server/project/service/application/workflow/states/DraftApplicationStateTest.kt
@@ -0,0 +1,147 @@
+package io.cloudflight.jems.server.project.service.application.workflow.states
+
+import io.cloudflight.jems.server.UnitTest
+import io.cloudflight.jems.server.audit.service.AuditService
+import io.cloudflight.jems.server.authentication.service.SecurityService
+import io.cloudflight.jems.server.project.service.ProjectPersistence
+import io.cloudflight.jems.server.project.service.ProjectWorkflowPersistence
+import io.cloudflight.jems.server.project.service.application.ApplicationActionInfo
+import io.cloudflight.jems.server.project.service.application.ApplicationStatus
+import io.cloudflight.jems.server.project.service.application.workflow.ApproveIsNotAllowedException
+import io.cloudflight.jems.server.project.service.application.workflow.ApproveWithConditionsIsNotAllowedException
+import io.cloudflight.jems.server.project.service.application.workflow.CallIsNotOpenException
+import io.cloudflight.jems.server.project.service.application.workflow.RefuseIsNotAllowedException
+import io.cloudflight.jems.server.project.service.application.workflow.ReturnToApplicantIsNotAllowedException
+import io.cloudflight.jems.server.project.service.application.workflow.RevertLastActionOnApplicationIsNotAllowedException
+import io.cloudflight.jems.server.project.service.application.workflow.SetAsEligibleIsNotAllowedException
+import io.cloudflight.jems.server.project.service.application.workflow.SetAsIneligibleIsNotAllowedException
+import io.cloudflight.jems.server.project.service.model.ProjectCallSettings
+import io.cloudflight.jems.server.project.service.model.ProjectSummary
+import io.mockk.every
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.impl.annotations.MockK
+import io.mockk.impl.annotations.RelaxedMockK
+import io.mockk.verify
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import java.time.LocalDate
+import java.time.ZonedDateTime
+
+class DraftApplicationStateTest : UnitTest() {
+
+    companion object {
+        private const val PROJECT_ID = 1L
+        private const val USER_ID = 7L
+        private const val CALL_ID = 10L
+
+        private val projectCallSettings = ProjectCallSettings(
+            callId = CALL_ID,
+            callName = "dummy call",
+            startDate = ZonedDateTime.now().minusDays(1),
+            endDate = ZonedDateTime.now().plusDays(1),
+            lengthOfPeriod = 0,
+            isAdditionalFundAllowed = false,
+            flatRates = emptySet(),
+            lumpSums = emptyList(),
+            unitCosts = emptyList(),
+        )
+
+        private val actionInfo = ApplicationActionInfo(
+            note = "some dummy note",
+            date = LocalDate.now()
+        )
+    }
+
+    @MockK
+    lateinit var projectSummary: ProjectSummary
+
+    @MockK
+    lateinit var projectWorkflowPersistence: ProjectWorkflowPersistence
+
+    @RelaxedMockK
+    lateinit var auditService: AuditService
+
+    @MockK
+    lateinit var securityService: SecurityService
+
+    @MockK
+    lateinit var projectPersistence: ProjectPersistence
+
+    @InjectMockKs
+    private lateinit var draftApplicationState: DraftApplicationState
+
+    @BeforeAll
+    fun setup() {
+        every { projectSummary.id } returns PROJECT_ID
+        every { projectSummary.status } returns ApplicationStatus.DRAFT
+        every { securityService.getUserIdOrThrow() } returns USER_ID
+    }
+
+    @Test
+    fun `submit - successful`() {
+        every { projectPersistence.getProjectCallSettings(PROJECT_ID) } returns projectCallSettings
+        every { projectWorkflowPersistence.updateApplicationFirstSubmission(any(), any()) } returns ApplicationStatus.SUBMITTED
+
+        assertThat(draftApplicationState.submit()).isEqualTo(ApplicationStatus.SUBMITTED)
+        verify(exactly = 1) { projectWorkflowPersistence.updateApplicationFirstSubmission(PROJECT_ID, USER_ID)  }
+    }
+
+    @Test
+    fun `submit - call is not yet open`() {
+        every { projectPersistence.getProjectCallSettings(PROJECT_ID) } returns projectCallSettings.copy(
+            startDate = ZonedDateTime.now().plusDays(1)
+        )
+        assertThrows<CallIsNotOpenException> { draftApplicationState.submit() }
+    }
+
+    @Test
+    fun `submit - call is already closed`() {
+        every { projectPersistence.getProjectCallSettings(PROJECT_ID) } returns projectCallSettings.copy(
+            endDate = ZonedDateTime.now().minusDays(1)
+        )
+        assertThrows<CallIsNotOpenException> { draftApplicationState.submit() }
+    }
+
+    @Test
+    fun setAsEligible() {
+        assertThrows<SetAsEligibleIsNotAllowedException> { draftApplicationState.setAsEligible(actionInfo) }
+    }
+
+    @Test
+    fun setAsIneligible() {
+        assertThrows<SetAsIneligibleIsNotAllowedException> { draftApplicationState.setAsIneligible(actionInfo) }
+    }
+
+    @Test
+    fun approve() {
+        assertThrows<ApproveIsNotAllowedException> { draftApplicationState.approve(actionInfo) }
+    }
+
+    @Test
+    fun approveWithConditions() {
+        assertThrows<ApproveWithConditionsIsNotAllowedException> { draftApplicationState.approveWithConditions(actionInfo) }
+    }
+
+    @Test
+    fun returnToApplicant() {
+        assertThrows<ReturnToApplicantIsNotAllowedException> { draftApplicationState.returnToApplicant() }
+    }
+
+    @Test
+    fun revertDecision() {
+        assertThrows<RevertLastActionOnApplicationIsNotAllowedException> { draftApplicationState.revertDecision() }
+    }
+
+    @Test
+    fun refuse() {
+        assertThrows<RefuseIsNotAllowedException> { draftApplicationState.refuse(actionInfo) }
+    }
+
+    @Test
+    fun getPossibleStatusToRevertTo() {
+        assertThat(draftApplicationState.getPossibleStatusToRevertTo()).isNull()
+    }
+
+}
