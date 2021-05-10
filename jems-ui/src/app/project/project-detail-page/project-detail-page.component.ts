e705739f731633ad26ba2fb4a82a7820dc4581d9
Alexandru Trif <Alexandru.Trif@cloudflight.io>
2021-05-10 16:37:26 +0300

    MP2-1519 Pre validation check removed from programme user

--- jems-ui/src/app/project/project-detail-page/project-detail-page.component.ts
+++ jems-ui/src/app/project/project-detail-page/project-detail-page.component.ts
@@ -1,6 +1,6 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {ProjectDetailPageStore} from './project-detail-page-store';
-import {OutputProjectFile, ProjectStatusDTO} from '@cat/api';
+import {OutputProjectFile, ProjectStatusDTO, UserRoleDTO} from '@cat/api';
 import {ProjectApplicationFormSidenavService} from '../project-application/containers/project-application-form-page/services/project-application-form-sidenav.service';
 import {ActivatedRoute} from '@angular/router';
 import {Permission} from '../../security/permissions/permission';
@@ -16,6 +16,8 @@ export class ProjectDetailPageComponent {
   fileType = OutputProjectFile.TypeEnum;
   Permission = Permission;
   STATUS = ProjectStatusDTO.StatusEnum;
+  // tslint:disable-next-line:variable-name
+  Permissions = UserRoleDTO.PermissionsEnum;
 
   projectId = this.activatedRoute.snapshot.params.projectId;
 
