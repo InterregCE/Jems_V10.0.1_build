d1dc7ed472a038b47f9d96d2639f07c5d0f72c7d
Mihai Rinzis <mihai@rmi.cat>
2021-05-11 12:28:41 +0300

    fix roles in sidenav are not refreshed

--- jems-ui/src/app/system/services/system-page-sidenav.service.ts
+++ jems-ui/src/app/system/services/system-page-sidenav.service.ts
@@ -1,10 +1,10 @@
 import {Injectable} from '@angular/core';
 import {SideNavService} from '@common/components/side-nav/side-nav.service';
 import {RoutingService} from '../../common/services/routing.service';
-import {filter, map, switchMap, tap} from 'rxjs/operators';
+import {filter, map, startWith, switchMap, tap} from 'rxjs/operators';
 import {UntilDestroy, untilDestroyed} from '@ngneat/until-destroy';
 import {PermissionService} from '../../security/permissions/permission.service';
-import {combineLatest, of} from 'rxjs';
+import {combineLatest, of, Subject} from 'rxjs';
 import {HeadlineRoute} from '@common/components/side-nav/headline-route';
 import {UserRoleDTO, UserRoleService, UserRoleSummaryDTO} from '@cat/api';
 import {Tables} from '../../common/utils/tables';
@@ -17,22 +17,41 @@ import PermissionsEnum = UserRoleDTO.PermissionsEnum;
 export class SystemPageSidenavService {
   public static SYSTEM_DETAIL_PATH = '/app/system';
 
+  rolesChanged$ = new Subject<void>();
+
+  constructor(private sideNavService: SideNavService,
+              private routingService: RoutingService,
+              private permissionService: PermissionService,
+              private roleService: UserRoleService) {
+    combineLatest([
+      this.routing$,
+      this.permissionService.permissionsChanged(),
+      this.roles$
+    ]).pipe(
+      tap(([routing, permissions, roles]) =>
+        this.setHeadlines(permissions as PermissionsEnum[], roles)),
+      untilDestroyed(this)
+    ).subscribe();
+  }
+
   private routing$ = this.routingService.routeChanges(SystemPageSidenavService.SYSTEM_DETAIL_PATH)
     .pipe(
       filter(systemPath => systemPath)
     );
 
-  private roles$ = this.permissionService.permissionsChanged()
-    .pipe(
-      switchMap(perms =>
-        perms.includes(PermissionsEnum.RoleRetrieve) ? this.roleService.list(
-          Tables.DEFAULT_INITIAL_PAGE_INDEX,
-          Tables.DEFAULT_INITIAL_PAGE_SIZE,
-          Tables.DEFAULT_INITIAL_SORT.active
-        ) : of({content: []})),
-      map(page => page?.content),
-      tap(roles => Log.info('Fetched roles for sidenav', this, roles))
-    );
+  private roles$ = combineLatest([
+    this.permissionService.permissionsChanged(),
+    this.rolesChanged$.pipe(startWith(null))
+  ]).pipe(
+    switchMap(([perms]) =>
+      perms.includes(PermissionsEnum.RoleRetrieve) ? this.roleService.list(
+        Tables.DEFAULT_INITIAL_PAGE_INDEX,
+        Tables.DEFAULT_INITIAL_PAGE_SIZE,
+        Tables.DEFAULT_INITIAL_SORT.active
+      ) : of({content: []})),
+    map(page => page?.content),
+    tap(roles => Log.info('Fetched roles for sidenav', this, roles))
+  );
 
   private setHeadlines(permissions: PermissionsEnum[], roles: UserRoleSummaryDTO[]): void {
     const bulletsArray: HeadlineRoute[] = [];
@@ -74,22 +93,6 @@ export class SystemPageSidenavService {
         bullets: bulletsArray
       },
     ]);
-  }
-
-  constructor(private sideNavService: SideNavService,
-              private routingService: RoutingService,
-              private permissionService: PermissionService,
-              private roleService: UserRoleService) {
-    combineLatest([
-      this.routing$,
-      this.permissionService.permissionsChanged(),
-      this.roles$
-    ]).pipe(
-      tap(([routing, permissions, roles]) =>
-        this.setHeadlines(permissions as PermissionsEnum[], roles)),
-      untilDestroyed(this)
-    )
-      .subscribe();
   }
 
 }
