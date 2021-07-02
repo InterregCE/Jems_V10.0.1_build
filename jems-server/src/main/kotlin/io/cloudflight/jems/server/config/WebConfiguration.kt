6280b4674daeb383e5de1e5df479dda057d493ff
Alexandru Trif <Alexandru.Trif@cloudflight.io>
2021-07-02 17:26:53 +0300

    MP2-1508 Override and refresh translation files

--- jems-server/src/main/kotlin/io/cloudflight/jems/server/config/WebConfiguration.kt
+++ jems-server/src/main/kotlin/io/cloudflight/jems/server/config/WebConfiguration.kt
@@ -8,6 +8,7 @@ import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerF
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpStatus
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
