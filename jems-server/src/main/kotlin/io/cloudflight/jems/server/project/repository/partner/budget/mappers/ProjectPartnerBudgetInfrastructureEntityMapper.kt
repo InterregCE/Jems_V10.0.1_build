5ed81e2adb5a7758803f80d20941c87ebe772251
Adrian Anghius <adrian.anghius@cloudflight.io>
2021-11-09 12:19:16 +0200

    MP2-1798 Add missing comments translatable field in budget entity mappers

--- jems-server/src/main/kotlin/io/cloudflight/jems/server/project/repository/partner/budget/mappers/ProjectPartnerBudgetInfrastructureEntityMapper.kt
+++ jems-server/src/main/kotlin/io/cloudflight/jems/server/project/repository/partner/budget/mappers/ProjectPartnerBudgetInfrastructureEntityMapper.kt
@@ -42,7 +42,7 @@ fun BudgetGeneralCostEntry.toProjectPartnerBudgetInfrastructureEntity(
                     unitType = unitType.extractTranslation(language),
                     comments = comments.extractTranslation(language)
                 )
-            }, arrayOf(description, awardProcedures, unitType)
+            }, arrayOf(description, awardProcedures, unitType, comments)
         )
 
         budgetPeriodEntities.addAll(budgetPeriods.map {
