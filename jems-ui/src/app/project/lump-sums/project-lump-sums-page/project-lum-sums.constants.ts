709e3055ae2a106904591e16c5a0199988d5508d
Mihai Rinzis <mihai@rmi.cat>
2022-01-13 15:41:37 +0200

    MP2-1583 fix lump sum max value

--- jems-ui/src/app/project/lump-sums/project-lump-sums-page/project-lum-sums.constants.ts
+++ jems-ui/src/app/project/lump-sums/project-lump-sums-page/project-lum-sums.constants.ts
@@ -2,7 +2,7 @@ export class ProjectLumSumsConstants {
 
 
   public static MIN_VALUE = 0;
-  public static MAX_VALUE = 999_999_999;
+  public static MAX_VALUE = 999_999_999.99;
   public static MAX_NUMBER_OF_ITEMS = 50;
   public static FORM_CONTROL_NAMES = {
     lumpSum: 'lumpSum',
