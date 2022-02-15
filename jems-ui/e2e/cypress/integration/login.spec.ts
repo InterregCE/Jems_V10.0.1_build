25c9308886c8eadcaf75c02da6ade92b1251e0ae
Jovan Ilić <jovan.ilic@cloudflight.io>
2022-02-15 12:26:48 +0000

    Add call tests, configuration and minor fixes

--- jems-ui/e2e/cypress/integration/login.spec.ts
+++ jems-ui/e2e/cypress/integration/login.spec.ts
@@ -1,45 +1,55 @@
+import faker from "@faker-js/faker";
+
 context('Login tests', () => {
-    beforeEach(() => {
-        cy.viewport(1920, 1080);
-    });
+  beforeEach(() => {
+    cy.viewport(1920, 1080);
+    cy.visit('/');
+  });
 
-    it('Admin can login and logout', () => {
-        cy.fixture('users.json').then((user) => {
-            cy.visit('/');
+  it('Admin can login and logout', () => {
+    cy.fixture('users.json').then((user) => {
 
-            cy.intercept('api/project/mine?*').as('applicationList');
+      cy.intercept('api/project/mine?*').as('applicationList');
 
-            cy.get('#email').type(user.admin.username);
-            cy.get('#password').type(Cypress.env('defaultPassword') + '{enter}');
+      cy.get('#email').type(user.admin.email);
+      cy.get('#password').type(Cypress.env('defaultPassword') + '{enter}');
 
-            cy.wait('@applicationList', { timeout: 10000 });
+      cy.wait('@applicationList', {timeout: 10000});
 
-            cy.get('h2').should('contain', 'My applications');
+      cy.get('h2').should('contain', 'My applications');
 
-            cy.intercept('api/auth/logout').as('logout');
-            cy.get('button.logout-button').click();
-            cy.wait('@logout');
+      cy.intercept('api/auth/logout').as('logout');
+      cy.get('button.logout-button').click();
+      cy.wait('@logout');
 
-            cy.get('button span').contains('Login').should('exist');
-        })
-    });
+      cy.get('span').should('contain', 'Login').and('be.visible');
+    })
+  });
 
-    it('Applicant can register', () => {
-        cy.visit('/');
+  it('Unknown user cannot login', () => {
 
-        cy.get('a').contains('Create a new account').click();
-        const uuid = () => Cypress._.random(0, 1e6)
-        const id = `${uuid()}`;
-        const email = `cypress.${id}@Applicant.eu`;
+    cy.get('#email').type('random_unknown_username');
+    cy.get('#password').type('random_unknown_password');
 
-        cy.get('input[name="name"]').type('Cypress');
-        cy.get('input[name="surname"]').type(id);
-        cy.get('input[name="email"]').type(email);
-        cy.get('input[name="password"]').type(email);
-        cy.get('mat-checkbox').click("left");
+    cy.get('button').contains('Login').click();
 
-        cy.get('button[color="primary"]').click();
+    cy.get('jems-alert').should('be.visible').and('contain', 'Email or password incorrect.');
+  });
 
-        cy.get('app-alert').find('span').contains('Go to login').should('be.visible');
-    });
+  it('Applicant can register', () => {
+
+    cy.get('a').contains('Create a new account').click();
+    const id = faker.random.alphaNumeric(5);
+    const email = `cypress.${id}@Applicant.eu`;
+
+    cy.get('input[name="name"]').type('Cypress');
+    cy.get('input[name="surname"]').type(id);
+    cy.get('input[name="email"]').type(email);
+    cy.get('input[name="password"]').type(email);
+    cy.get('mat-checkbox').click("left");
+
+    cy.get('button[color="primary"]').click();
+
+    cy.get('jems-alert').find('span').contains('Go to login').should('be.visible');
+  });
 })
