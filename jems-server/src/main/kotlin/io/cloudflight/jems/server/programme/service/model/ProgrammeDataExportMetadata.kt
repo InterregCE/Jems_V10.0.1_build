309765ca5cd38b1b356d3533b3bc3216912fb625
Mehran Haghani <mehran.haghani@cloudflight.io>
2022-02-23 10:25:34 +0100

    MP2-1531 - solves issues found in testing

--- jems-server/src/main/kotlin/io/cloudflight/jems/server/programme/service/model/ProgrammeDataExportMetadata.kt
+++ jems-server/src/main/kotlin/io/cloudflight/jems/server/programme/service/model/ProgrammeDataExportMetadata.kt
@@ -1,6 +1,7 @@
 package io.cloudflight.jems.server.programme.service.model
 
 import io.cloudflight.jems.api.programme.dto.language.SystemLanguage
+import io.cloudflight.jems.server.programme.service.exportProgrammeData.EXPORT_TIMEOUT_IN_MINUTES
 import java.time.ZonedDateTime
 
 data class ProgrammeDataExportMetadata(
@@ -12,4 +13,13 @@ data class ProgrammeDataExportMetadata(
     var requestTime: ZonedDateTime,
     var exportStartedAt: ZonedDateTime?,
     var exportEndedAt: ZonedDateTime?,
-)
+) {
+    fun isTimedOut() =
+        exportEndedAt == null && requestTime.isBefore(ZonedDateTime.now().minusMinutes(EXPORT_TIMEOUT_IN_MINUTES))
+
+    fun isFailed() =
+        exportEndedAt != null && fileName == null && contentType == null
+
+    fun isReadyToDownload() =
+        exportEndedAt != null && fileName != null && contentType != null
+}
