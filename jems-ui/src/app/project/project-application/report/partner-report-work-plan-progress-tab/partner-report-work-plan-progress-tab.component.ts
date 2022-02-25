2acf577b8014c7ac4d3c80b7c7426ea85b0ea8a9
mmihailescu <mihnea.mihailescu@cloudflight.io>
2022-02-25 10:56:42 +0200

    MP2-2278 Added FE for reporting workplan

--- jems-ui/src/app/project/project-application/report/partner-report-work-plan-progress-tab/partner-report-work-plan-progress-tab.component.ts
+++ jems-ui/src/app/project/project-application/report/partner-report-work-plan-progress-tab/partner-report-work-plan-progress-tab.component.ts
@@ -1,22 +1,25 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
-import {FormBuilder, FormGroup} from '@angular/forms';
+import {FormArray, FormBuilder, FormGroup} from '@angular/forms';
 import {FormService} from '@common/components/section/form/form.service';
 import {
   ProjectApplicationFormSidenavService
 } from '@project/project-application/containers/project-application-form-page/services/project-application-form-sidenav.service';
-import {
-  ProjectPartnerReportPageStore
-} from '@project/project-application/report/project-partner-report-page-store.service';
+import {PartnerReportPageStore} from '@project/project-application/report/partner-report-page-store.service';
 import {combineLatest, Observable} from 'rxjs';
 import {
-  OutputWorkPackage, OutputWorkPackageSimple,
-  ProjectPeriodDTO,
-  WorkPackageActivityDTO,
-  WorkPackageInvestmentDTO,
-  WorkPackageOutputDTO
+  ProjectPartnerReportWorkPackageActivityDeliverableDTO,
+  ProjectPartnerReportWorkPackageActivityDTO,
+  ProjectPartnerReportWorkPackageDTO,
+  ProjectPartnerReportWorkPackageOutputDTO,
+  WorkPackageService
 } from '@cat/api';
-import {ProjectPartner} from '@project/model/ProjectPartner';
-import {map, tap} from 'rxjs/operators';
+import {catchError, map, take, tap} from 'rxjs/operators';
+import {
+  PartnerReportWorkPlanPageStore
+} from '@project/project-application/report/partner-report-work-plan-progress-tab/partner-report-work-plan-page-store.service';
+import {
+  PartnerReportWorkplanConstants
+} from '@project/project-application/report/partner-report-work-plan-progress-tab/partner-report-work-plan-progress.constants';
 
 @Component({
   selector: 'jems-partner-report-work-plan-progress-tab',
@@ -26,26 +29,116 @@ import {map, tap} from 'rxjs/operators';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PartnerReportWorkPlanProgressTabComponent {
-  data$: Observable<{
-    workPackages: OutputWorkPackageSimple[];
-  }>;
 
-  form: FormGroup = this.formBuilder.group({
+  constants = PartnerReportWorkplanConstants;
+  savedWorkPackages$: Observable<ProjectPartnerReportWorkPackageDTO[]>;
+
+  workPlanForm: FormGroup = this.formBuilder.group({
+    workPackages: this.formBuilder.array([ this.formBuilder.group({
+      id: this.formBuilder.control(''),
+      description: this.formBuilder.control([], this.constants.WORK_PACKAGE_DESCRIPTION.validators),
+      activities: this.formBuilder.array([]),
+      outputs: this.formBuilder.array([]),
+    })])
   });
 
   constructor(private formBuilder: FormBuilder,
               private formService: FormService,
               private projectSidenavService: ProjectApplicationFormSidenavService,
-              public projectPartnerReportPageStore: ProjectPartnerReportPageStore) {
+              public partnerReportPageStore: PartnerReportPageStore,
+              private workPackageService: WorkPackageService,
+              private pageStore: PartnerReportWorkPlanPageStore) {
 
-    // this.data$ = combineLatest([
-    //   this.projectPartnerReportPageStore.getWorkPackages(),
-    // ])
-    //   .pipe(
-    //     map(([workPackages]) => ({
-    //       workPackages: workPackages
-    //     }))
-    //   );
+    this.savedWorkPackages$ = this.pageStore.partnerWorkPackages$
+      .pipe(
+        tap((workPackages) => this.resetForm(workPackages))
+      );
   }
 
+  get workPackages(): FormArray {
+    return this.workPlanForm.get(this.constants.WORK_PACKAGES.name) as FormArray;
+  }
+
+  activities(workPackageIndex: number): FormArray {
+    return this.workPackages.at(workPackageIndex).get(this.constants.ACTIVITIES.name) as FormArray;
+  }
+
+  deliverables(workPackageIndex:number, activityIndex: number): FormArray {
+    return this.activities(workPackageIndex).at(activityIndex).get(this.constants.DELIVERABLES.name) as FormArray;
+  }
+
+  outputs(workPackageIndex: number): FormArray {
+    return this.workPackages.at(workPackageIndex).get(this.constants.OUTPUTS.name) as FormArray;
+  }
+
+
+  resetForm(workPackages: ProjectPartnerReportWorkPackageDTO[]) {
+    this.workPlanForm.reset();
+    this.workPackages.clear();
+    this.workPlanForm = this.formBuilder.group({
+      workPackages: this.formBuilder.array([])
+    });
+
+    workPackages.forEach((workPackage: ProjectPartnerReportWorkPackageDTO, index: number) => {
+      this.resetWorkPackage(workPackage, index)
+    })
+  }
+
+  resetWorkPackage(workPackage: ProjectPartnerReportWorkPackageDTO, workPackageIndex: number): void {
+    this.workPackages.push(  this.formBuilder.group({
+      id: this.formBuilder.control(workPackage.id),
+      description: this.formBuilder.control(workPackage.description, this.constants.WORK_PACKAGE_DESCRIPTION.validators),
+      activities: this.formBuilder.array([]),
+      outputs: this.formBuilder.array([]),
+    }));
+
+    workPackage.activities.forEach((activity: ProjectPartnerReportWorkPackageActivityDTO, activityIndex: number) => {
+      this.addActivity(workPackageIndex, activity);
+      activity.deliverables?.forEach((deliverable: ProjectPartnerReportWorkPackageActivityDeliverableDTO) => this.addDeliverable(workPackageIndex, activityIndex, deliverable));
+    });
+
+    workPackage.outputs.forEach((output: ProjectPartnerReportWorkPackageOutputDTO) => {
+      this.addOutput(workPackageIndex, output);
+    });
+
+    this.formService.init(this.workPlanForm);
+  }
+
+  private addActivity(workPackageIndex: number, existing?: ProjectPartnerReportWorkPackageActivityDTO): void {
+    this.activities(workPackageIndex).push(this.formBuilder.group(
+      {
+        id: existing?.id,
+        title: this.formBuilder.control(existing?.title || []),
+        progress: this.formBuilder.control(existing?.progress || [], this.constants.ACTIVITY_PROGRESS.validators),
+        deliverables: this.formBuilder.array([]),
+      })
+    );
+  }
+
+  private addDeliverable(workPackageIndex:number, activityIndex: number, existing?: ProjectPartnerReportWorkPackageActivityDeliverableDTO): void {
+    this.deliverables(workPackageIndex, activityIndex).push(this.formBuilder.group({
+      id: existing?.id,
+      title: this.formBuilder.control(existing?.title || []),
+      contribution: this.formBuilder.control(existing?.contribution || false),
+      evidence: this.formBuilder.control(existing?.evidence || false),
+    }));
+  }
+
+  private addOutput(workPackageIndex: number, existing?: ProjectPartnerReportWorkPackageOutputDTO): void {
+    this.outputs(workPackageIndex).push(this.formBuilder.group({
+      id: existing?.id,
+      title: this.formBuilder.control(existing?.title || []),
+      contribution: this.formBuilder.control(existing?.contribution || false),
+      evidence: this.formBuilder.control(existing?.evidence || false),
+    }));
+  }
+
+  saveWorkPlan(): void {
+    this.pageStore.saveWorkPackages(this.workPackages.value)
+      .pipe(
+        take(1),
+        tap(() => this.formService.setSuccess('project.work.package.tab.activities.saved')),
+        catchError(err => this.formService.setError(err))
+      ).subscribe();
+  }
 }
