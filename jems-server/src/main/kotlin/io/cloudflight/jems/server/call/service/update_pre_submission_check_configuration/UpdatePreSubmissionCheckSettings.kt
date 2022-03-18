74d480a230b2e3db924711efb5eddbbd430414fb
Ondrej Oravčok <Ondrej.Oravcok@cloudflight.io>
2022-03-18 09:21:18 +0100

    MP2-2309 fix ignored validation when setting plugins

--- jems-server/src/main/kotlin/io/cloudflight/jems/server/call/service/update_pre_submission_check_configuration/UpdatePreSubmissionCheckSettings.kt
+++ jems-server/src/main/kotlin/io/cloudflight/jems/server/call/service/update_pre_submission_check_configuration/UpdatePreSubmissionCheckSettings.kt
@@ -3,6 +3,7 @@ package io.cloudflight.jems.server.call.service.update_pre_submission_check_conf
 import io.cloudflight.jems.plugin.contract.pre_condition_check.PreConditionCheckPlugin
 import io.cloudflight.jems.server.call.authorization.CanUpdateCall
 import io.cloudflight.jems.server.call.service.CallPersistence
+import io.cloudflight.jems.server.call.service.model.CallDetail
 import io.cloudflight.jems.server.call.service.model.PreSubmissionPlugins
 import io.cloudflight.jems.server.common.exception.ExceptionWrapper
 import io.cloudflight.jems.server.plugin.JemsPluginRegistry
@@ -18,13 +19,17 @@ class UpdatePreSubmissionCheckSettings(
     @CanUpdateCall
     @Transactional
     @ExceptionWrapper(UpdatePreSubmissionCheckSettingsException::class)
-    override fun update(callId: Long, pluginKeys: PreSubmissionPlugins) =
-        ifSelectedPluginKeyIsValid(pluginKeys).run {
-            persistence.updateProjectCallPreSubmissionCheckPlugin(callId, pluginKeys)
-        }
+    override fun update(callId: Long, pluginKeys: PreSubmissionPlugins): CallDetail {
+        val call = persistence.getCallById(callId)
 
-    private fun ifSelectedPluginKeyIsValid(pluginKeys: PreSubmissionPlugins): Boolean {
-        if (pluginKeys.callHasTwoSteps) {
+        return if (isSelectedPluginKeyValid(call.is2StepCall(), pluginKeys))
+            persistence.updateProjectCallPreSubmissionCheckPlugin(callId, pluginKeys)
+        else
+            call
+    }
+
+    private fun isSelectedPluginKeyValid(hasTwoSteps: Boolean, pluginKeys: PreSubmissionPlugins): Boolean {
+        if (hasTwoSteps) {
             return jemsPluginRegistry.get(PreConditionCheckPlugin::class, pluginKeys.pluginKey).getKey().isNotEmpty()
                 && jemsPluginRegistry.get(PreConditionCheckPlugin::class, pluginKeys.firstStepPluginKey).getKey().isNotEmpty()
         }
