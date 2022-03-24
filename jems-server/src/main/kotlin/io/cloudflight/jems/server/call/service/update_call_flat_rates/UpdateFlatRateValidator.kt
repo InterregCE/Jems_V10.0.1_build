3d3a20d2d113e42380cb8098ed82ae14642da3c3
aanghius <adrian.anghius@cloudflight.io>
2022-03-24 20:08:49 +0200

    MP2-2382 Call setting: Lift maximum for all Flat rates

--- jems-server/src/main/kotlin/io/cloudflight/jems/server/call/service/update_call_flat_rates/UpdateFlatRateValidator.kt
+++ jems-server/src/main/kotlin/io/cloudflight/jems/server/call/service/update_call_flat_rates/UpdateFlatRateValidator.kt
@@ -1,18 +1,11 @@
 package io.cloudflight.jems.server.call.service.update_call_flat_rates
 
-import io.cloudflight.jems.api.call.dto.flatrate.FlatRateType
 import io.cloudflight.jems.api.common.dto.I18nMessage
 import io.cloudflight.jems.server.call.service.model.ProjectCallFlatRate
 
 private const val OUT_OF_RANGE_ERR = "call.flatRateSetup.rate.out.of.range"
-
-private val maxFlatRates = mapOf(
-    FlatRateType.STAFF_COSTS to 20,
-    FlatRateType.OFFICE_AND_ADMINISTRATION_ON_STAFF_COSTS to 15,
-    FlatRateType.OFFICE_AND_ADMINISTRATION_ON_OTHER_COSTS to 25,
-    FlatRateType.TRAVEL_AND_ACCOMMODATION_ON_STAFF_COSTS to 15,
-    FlatRateType.OTHER_COSTS_ON_STAFF_COSTS to 40
-)
+private const val MAX_FLAT_RATE = 100
+private const val MIN_FLAT_RATE = 1
 
 fun validateFlatRates(flatRates: Set<ProjectCallFlatRate>) {
     val groupedByType = flatRates.associateBy { it.type }
@@ -20,13 +13,9 @@ fun validateFlatRates(flatRates: Set<ProjectCallFlatRate>) {
         throw DuplicateFlatRateTypesDefined()
     }
     val errors = flatRates
-        .filter { it.rate > getMaxRateForType(it.type) || it.rate < 1 }
+        .filter { it.rate > MAX_FLAT_RATE || it.rate < MIN_FLAT_RATE }
         .associateBy({ it.type.name }, { I18nMessage(i18nKey = OUT_OF_RANGE_ERR) })
 
     if (errors.isNotEmpty())
         throw FlatRateOutOfBounds(errors)
-}
-
-private fun getMaxRateForType(type: FlatRateType): Int {
-    return maxFlatRates.getValue(type)
 }
