b7d1e49fb53495ac9dfc37deed7d86aa1d84df92
mmihailescu <mihnea.mihailescu@cloudflight.io>
2022-03-30 17:46:04 +0300

    MP2-2424 Added new supported extensions

--- jems-ui/src/app/project/common/components/file-management/project-application-files-table/project-application-files-table.component.ts
+++ jems-ui/src/app/project/common/components/file-management/project-application-files-table/project-application-files-table.component.ts
@@ -22,7 +22,7 @@ export class ProjectApplicationFilesTableComponent {
   Alert = Alert;
   Tables = Tables;
 
-  acceptedFilesTypes = ['.csv', '.dat', '.db', '.dbf', '.log', '.mdb', '.xml', '.email', '.eml', '.emlx', '.msg', '.oft', '.ost', '.pst', '.vcf', '.bmp', '.gif', '.jpeg', '.jpg', '.png', '.psd', '.svg', '.tif', '.tiff', '.htm', '.html', '.key', '.odp', '.pps', '.ppt', '.ppt', '.pptx', '.ods', '.xls', '.xlsm', '.xlsx', '.doc', '.docx', '.odt', '.pdf', '.rtf', '.tex', '.txt', '.wpd', '.mov', '.avi', '.mp4', '.zip', '.rar', '.ace', '.7z', '.url'];
+  acceptedFilesTypes = ['.ddoc', '.asice', '.asics', '.adoc', '.bdoc', '.edoc', '.sce', '.scs', '.csv', '.dat', '.db', '.dbf', '.log', '.mdb', '.xml', '.email', '.eml', '.emlx', '.msg', '.oft', '.ost', '.pst', '.vcf', '.bmp', '.gif', '.jpeg', '.jpg', '.png', '.psd', '.svg', '.tif', '.tiff', '.htm', '.html', '.key', '.odp', '.pps', '.ppt', '.ppt', '.pptx', '.ods', '.xls', '.xlsm', '.xlsx', '.doc', '.docx', '.odt', '.pdf', '.rtf', '.tex', '.txt', '.wpd', '.mov', '.avi', '.mp4', '.zip', '.rar', '.ace', '.7z', '.url'];
   displayedColumns: string[] = ['name', 'uploadDate', 'user', 'description', 'actions'];
   dataSource = new MatTableDataSource<ProjectFileMetadataDTO>();
   maximumAllowedFileSizeInMB: number;
