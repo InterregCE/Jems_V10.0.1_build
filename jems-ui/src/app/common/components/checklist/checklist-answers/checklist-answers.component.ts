ec8ff2d28dff9935e5c9e25e5824669737a96b7e
Mihai Rinzis <mihai@rmi.cat>
2022-04-11 15:10:51 +0300

    MP2-2385 add checklist preview warning

--- jems-ui/src/app/common/components/checklist/checklist-answers/checklist-answers.component.ts
+++ jems-ui/src/app/common/components/checklist/checklist-answers/checklist-answers.component.ts
@@ -5,6 +5,7 @@ import {
   ProgrammeChecklistComponentDTO,
   ProgrammeChecklistDetailDTO
 } from '@cat/api';
+import {Alert} from '@common/components/forms/alert';
 
 @Component({
   selector: 'jems-checklist-answers',
@@ -14,9 +15,13 @@ import {
 })
 export class ChecklistAnswersComponent {
   COMPONENT_TYPE = ProgrammeChecklistComponentDTO.TypeEnum;
+  Alert = Alert;
 
   @Input()
   checklist: ProgrammeChecklistDetailDTO;
+
+  @Input()
+  dirty = false;
 
   getHeadline(component: ProgrammeChecklistComponentDTO): HeadlineMetadataDTO {
     return component.metadata as HeadlineMetadataDTO;
