e16df76f0ce3e8d2863d739d595354dd7bb542fb
Jovan <jovan.ilic@cloudflight.io>
2022-05-09 17:02:09 +0200

    TB-581 Applicant can add lead partner to the project

--- jems-ui/e2e/cypress/integration/project/application-form/application.spec.ts
+++ jems-ui/e2e/cypress/integration/project/application-form/application.spec.ts
@@ -19,7 +19,7 @@ context('Project management tests', () => {
     cy.loginByRequest(user.applicantUser.email);
   });
 
-  it('TB-390 Applicant can apply for a call', function () {
+  it('TB-390 Applicant can apply for a call', () => {
     cy.visit('/');
 
     cy.contains('Call list').should('be.visible');
@@ -34,9 +34,14 @@ context('Project management tests', () => {
     cy.contains('Create project application').click();
 
     cy.get('jems-project-page-template').find('h1').contains(`${application.details.acronym}`);
+    cy.location('pathname').then(path => {
+      const regex = /detail\/(\d+)\//;
+      const match = path.match(regex);
+      cy.wrap(match[1]).as('applicationId');
+    });
   });
 
-  it('TB-556 Applicant can open and edit his projects', function () {
+  it('TB-556 Applicant can open and edit his projects', () => {
     cy.fixture('project/application-form/TB-390').then(testData => {
 
       cy.visit('/');
@@ -74,6 +79,46 @@ context('Project management tests', () => {
 
       cy.contains('Save changes').click();
       cy.contains('Project identification saved successfully.').should('exist');
+    });
+  });
+
+  it('TB-581 Applicant can add partner to the project', function () {
+    cy.fixture('project/application-form/TB-581').then(testData => {
+      cy.visit(`app/project/detail/${this.applicationId}`, {failOnStatusCode: false});
+      cy.contains('Partners overview').click();
+      cy.contains('Add new partner').click();
+      const leadPartner = testData.partner;
+      cy.contains('button', leadPartner.role).click();
+      cy.contains('div', 'Abbreviated name of the organisation').find('input').type(leadPartner.abbreviation);
+      cy.contains('div', 'Name of the organisation in original language').find('input').type(leadPartner.nameInOriginalLanguage);
+      cy.contains('div', 'Name of the organisation in english').find('input').type(leadPartner.nameInEnglish);
+      cy.contains('div', 'Department').find('textarea').type(leadPartner.nameInEnglish);
+
+      cy.contains('div', 'Type of partner').find('mat-select').click();
+      cy.contains('mat-option', leadPartner.partnerType).click();
+      cy.contains('div', 'Subtype of partner').find('mat-select').click();
+      cy.contains('mat-option', leadPartner.partnerSubType).click();
+      cy.contains('div', 'Legal status').find('mat-select').click();
+      cy.contains('mat-option', leadPartner.legalStatus).click();
+      cy.contains('div', 'Sector of activity at NACE group level').find('input').click();
+      cy.contains('mat-option', leadPartner.nace).click();
+
+      cy.contains('div', 'VAT number').find('input').type(leadPartner.vat);
+      cy.get('mat-button-toggle-group[formcontrolname="vatRecovery"]').contains(leadPartner.vatRecovery).click();
+      cy.contains('div', 'Other identifier number').find('input').type(leadPartner.otherIdentifierNumber);
+      cy.contains('div', 'Other identifier number').find('input').type(leadPartner.otherIdentifierNumber);
+      leadPartner.otherIdentifierDescription.forEach(description => {
+        cy.contains('jems-multi-language-container', 'Other identifier description').then(el => {
+          cy.wrap(el).contains('button', description.language).click();
+          cy.wrap(el).find('input').type(description.translation);
+        });
+      });
+      cy.contains('div', 'PIC').find('input').type(leadPartner.pic);
+
+      cy.contains('button', 'Create').click();
+      cy.contains('div', 'Project partners').should(partnersSection => {
+        expect(partnersSection).to.contain(leadPartner.abbreviation);
+      });
     });
   });
 })
