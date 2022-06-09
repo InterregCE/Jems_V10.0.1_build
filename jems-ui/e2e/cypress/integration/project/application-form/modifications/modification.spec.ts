ab011f83849e9c2a37262d740c1c35faee18ec9a
Jovan <jovan.ilic@cloudflight.io>
2022-06-09 13:42:03 +0200

    Fix application acronym size. Temporary fix for detached error

--- jems-ui/e2e/cypress/integration/project/application-form/modifications/modification.spec.ts
+++ jems-ui/e2e/cypress/integration/project/application-form/modifications/modification.spec.ts
@@ -40,6 +40,8 @@ context('Application modifications', () => {
         cy.get('jems-modification-confirmation').contains('div', 'Entry into force (MM/DD/YYYY)').find('input').type(testData.approved.entryIntoForceDate);
         cy.get('jems-modification-confirmation').contains('div', 'Explanatory notes').find('textarea').type(testData.approved.note);
         cy.get('jems-modification-confirmation').contains('Save changes').click();
+        
+        cy.wait(1000); // TODO remove after MP2-2391 is fixed
 
         cy.contains('Past modifications').next().should(pastModificationsSection => {
           expect(pastModificationsSection).to.contain('Approved');
@@ -73,6 +75,8 @@ context('Application modifications', () => {
 
       cy.contains('Save changes').click();
 
+      cy.wait(1000); // TODO remove after MP2-2391 is fixed
+
       cy.contains('Past modifications').next().should(pastModificationsSection => {
         expect(pastModificationsSection).to.contain('Rejected');
         expect(pastModificationsSection).to.contain('Modification 1');
@@ -102,9 +106,11 @@ context('Application modifications', () => {
       cy.get('jems-project-application-information').find('div').should('contain.text', 'Modification precontracted submitted');
 
       cy.contains('.link', 'A - Project identification').click();
+      cy.wait(1000); // TODO remove after MP2-2391 is fixed
       cy.get('textarea').should('have.value', 'New title');
 
       cy.contains('(current) V. 2.0').click();
+      cy.wait(1000); // TODO remove after MP2-2391 is fixed
       cy.contains('V. 1.0').should('be.visible').click();
       cy.contains('You are currently viewing an old version of this application').should('be.visible');
       cy.get('textarea').should('have.value', 'API generated application title DE');
