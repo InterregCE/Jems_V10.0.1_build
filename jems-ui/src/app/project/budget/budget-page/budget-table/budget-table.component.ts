b94c46eaea1c0815279e58fde553d2565590abaf
awansch <alexander.wansch@cloudflight.io>
2022-07-21 12:50:54 +0200

    MP2-2679 Add hyperlinks to the budget page of the project partners in the AF

--- jems-ui/src/app/project/budget/budget-page/budget-table/budget-table.component.ts
+++ jems-ui/src/app/project/budget/budget-page/budget-table/budget-table.component.ts
@@ -137,4 +137,7 @@ export class BudgetTableComponent implements OnInit, OnChanges {
     return columns;
   }
 
+  scrollTo(elementId: string) {
+    document.getElementById(elementId)?.scrollIntoView({behavior:'smooth', block:'center'});
+  }
 }
