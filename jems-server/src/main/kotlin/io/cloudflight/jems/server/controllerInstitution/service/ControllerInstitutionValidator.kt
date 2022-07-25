5b3d4fa53b71676866dfb5cdd3f3e92704efa71b
aanghius <adrian.anghius@cloudflight.io>
2022-07-25 07:09:59 +0300

    MP2-2389 Controllers: Institutions Fixes  - add validation for users when creating a new institution  - updated permissions translations for controller institutions  - fixed failing tests and increased test coverage  - add validator and adapted code

---
+++ jems-server/src/main/kotlin/io/cloudflight/jems/server/controllerInstitution/service/ControllerInstitutionValidator.kt
@@ -0,0 +1,36 @@
+package io.cloudflight.jems.server.controllerInstitution.service
+
+import io.cloudflight.jems.server.controllerInstitution.service.model.ControllerInstitutionUser
+import io.cloudflight.jems.server.project.service.projectuser.assign_user_collaborator_to_project.UsersAreNotValid
+import io.cloudflight.jems.server.user.service.UserPersistence
+import io.cloudflight.jems.server.user.service.UserRolePersistence
+import io.cloudflight.jems.server.user.service.model.UserRolePermission
+import org.springframework.stereotype.Service
+
+@Service
+class ControllerInstitutionValidator(
+    private val userRolePersistence: UserRolePersistence,
+    private val userPersistence: UserPersistence
+) {
+
+    fun validateInstitutionUsers(newUsers: List<ControllerInstitutionUser>) {
+        val newInstitutionUsersEmails = newUsers.map { it.userEmail.lowercase() }.toMutableList()
+        val foundUserEmails = getUsersThatCanBePersisted(newInstitutionUsersEmails)
+        newInstitutionUsersEmails.removeAll(foundUserEmails)
+        if (newInstitutionUsersEmails.isNotEmpty())
+            throw UsersAreNotValid(emails = newInstitutionUsersEmails)
+    }
+
+    private fun getUsersThatCanBePersisted(newInstitutionUsersEmails: List<String>): List<String> {
+        val monitorRoleIds = getMonitorRoleIds()
+        return userPersistence.findAllByEmails(newInstitutionUsersEmails)
+            .filter { persistedUser -> persistedUser.userRole.id in monitorRoleIds }
+            .map { it.email.lowercase() }
+    }
+
+    private fun getMonitorRoleIds(): Set<Long> =
+        userRolePersistence.findRoleIdsHavingAndNotHavingPermissions(
+            needsToHaveAtLeastOneFrom = UserRolePermission.getProjectMonitorPermissions(),
+            needsNotToHaveAnyOf = emptySet(),
+        )
+}
