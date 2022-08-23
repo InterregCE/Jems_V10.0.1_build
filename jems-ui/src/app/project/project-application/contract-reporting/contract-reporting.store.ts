fd57d8192989a95b1c49f397e29f70b806f45024
CATALYSTS\uacar <utku.acar@cloudflight.io>
2022-08-23 13:39:15 +0300

    Contract scheduling UI update for improper previously added deadlines

--- jems-ui/src/app/project/project-application/contract-reporting/contract-reporting.store.ts
+++ jems-ui/src/app/project/project-application/contract-reporting/contract-reporting.store.ts
@@ -4,7 +4,7 @@ import {
   ProjectContractingMonitoringService,
   ProjectContractingReportingScheduleDTO,
   ProjectContractingReportingService,
-  ProjectDetailFormDTO, ProjectPeriodForMonitoringDTO,
+  ProjectPeriodForMonitoringDTO,
   ProjectUserCollaboratorDTO,
   UserRoleCreateDTO
 } from '@cat/api';
