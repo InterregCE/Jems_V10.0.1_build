c083441bad4fe53f04a63ef4972e7b0fe538a62a
mwizany <markus.wizany@cloudflight.io>
2022-08-26 14:45:26 +0200

    TB-734 Project lump sums can be defined and allocated to project partners

---
+++ jems-ui/e2e/cypress/e2e/project/application-form/project-lump-sums/project-lump-sums.spec.ts
@@ -0,0 +1,55 @@
+import user from '../../../../fixtures/users.json';
+import application from '../../../../fixtures/api/application/application.json';
+import call from "../../../../fixtures/api/call/1.step.call.json";
+import {faker} from "@faker-js/faker";
+
+context('Project lump sums tests', () => {
+  it('TB-734 Project lump sums can be defined and allocated to project partners', () => {
+    cy.loginByRequest(user.programmeUser.email);
+    call.budgetSettings.lumpSums.push(3);
+    cy.createCall(call).then(callId => {
+      application.details.projectCallId = callId;
+      cy.publishCall(callId);
+    })
+    cy.loginByRequest(user.applicantUser.email);
+    cy.createApplication(application).then(applicationId => {
+      cy.visit('/app/project/detail/'+applicationId+'/applicationFormPartner', {failOnStatusCode: false});
+      cy.contains('Add new partner').click();
+      cy.contains('button', 'Partner').click();
+      cy.contains('div.mat-form-field-flex', 'Abbreviated name of the organisation').type(faker.word.noun());
+      cy.contains('div.mat-form-field-flex', 'Legal status').click();
+      cy.contains('Public').click();
+      cy.contains('button', 'Create').click();
+      cy.visit('/app/project/detail/'+applicationId+'/applicationFormPartner', {failOnStatusCode: false});
+      cy.contains('Add new partner').click();
+      cy.contains('button', 'Partner').click();
+      cy.contains('div.mat-form-field-flex', 'Abbreviated name of the organisation').type(faker.word.noun());
+      cy.contains('div.mat-form-field-flex', 'Legal status').click();
+      cy.contains('Public').click();
+      cy.intercept('/api/programmeLegalStatus').as('PartnerCreation');
+      cy.contains('button', 'Create').click();
+      cy.wait(1000);
+
+      cy.contains('E.1 - Project lump sums').click();
+      cy.contains('button', 'Add').click();
+      cy.contains('div.mat-form-field-flex', 'Lump sum').click();
+      cy.contains('Closure Lump sum DE').click();
+      cy.get('input[type="decimal"]').first().clear().type('2000');
+      cy.get('input[type="decimal"]').last().clear().type('3555,56');
+      cy.contains('Total lump sum costs must be allocated to one partner').should('be.visible');
+      cy.get('input[type="decimal"]').last().clear();
+      cy.get('input[type="decimal"]').first().clear().type('5550');
+      cy.contains('Please update the lump sum table: The sum of the amounts per partner must match the total lump sum costs.').scrollIntoView().should('be.visible');
+      cy.get('input[type="decimal"]').first().clear().type('5555,56');
+      cy.contains('button', 'Save changes').click();
+      cy.contains("Project's LumpSums saved successfully").should('be.visible');
+      cy.contains('button', 'add').click();
+      cy.get('mat-row').last().contains('span.mat-select-placeholder', 'Lump sum').click();
+      cy.contains('Preparation Lump sum DE').click();
+      cy.get('mat-row').last().find('input[type="decimal"]').first().clear().type('20000');
+      cy.get('mat-row').last().find('input[type="decimal"]').last().clear().type('35555,56');
+      cy.contains('button', 'Save changes').click();
+      cy.contains("Project's LumpSums saved successfully").should('be.visible');
+    });
+  })
+});
