e9e5d8c9a8a98f5b9c8ed722684fe0534e6ddc55
mwizany <markus.wizany@cloudflight.io>
2022-08-29 10:29:40 +0200

    TB-734 changed partner creation to use api

--- jems-ui/e2e/cypress/e2e/project/application-form/project-lump-sums/project-lump-sums.spec.ts
+++ jems-ui/e2e/cypress/e2e/project/application-form/project-lump-sums/project-lump-sums.spec.ts
@@ -1,7 +1,6 @@
 import user from '../../../../fixtures/users.json';
 import application from '../../../../fixtures/api/application/application.json';
 import call from "../../../../fixtures/api/call/1.step.call.json";
-import {faker} from "@faker-js/faker";
 
 context('Project lump sums tests', () => {
   it('TB-734 Project lump sums can be defined and allocated to project partners', () => {
@@ -14,22 +13,10 @@ context('Project lump sums tests', () => {
       })
       cy.loginByRequest(user.applicantUser.email);
       cy.createApplication(application).then(applicationId => {
-        cy.visit('/app/project/detail/'+applicationId+'/applicationFormPartner', {failOnStatusCode: false});
-        cy.contains('Add new partner').click();
-        cy.contains('button', 'Partner').click();
-        cy.contains('div.mat-form-field-flex', 'Abbreviated name of the organisation').type(faker.word.noun());
-        cy.contains('div.mat-form-field-flex', 'Legal status').click();
-        cy.contains('Public').click();
-        cy.contains('button', 'Create').click();
-        cy.visit('/app/project/detail/'+applicationId+'/applicationFormPartner', {failOnStatusCode: false});
-        cy.contains('Add new partner').click();
-        cy.contains('button', 'Partner').click();
-        cy.contains('div.mat-form-field-flex', 'Abbreviated name of the organisation').type(faker.word.noun());
-        cy.contains('div.mat-form-field-flex', 'Legal status').click();
-        cy.contains('Public').click();
-        cy.intercept('/api/programmeLegalStatus').as('PartnerCreation');
-        cy.contains('button', 'Create').click();
-        cy.wait(1000);
+        cy.updateProjectIdentification(applicationId, application.identification);
+        cy.createPartners(applicationId, testData.partners);
+
+        cy.visit('/app/project/detail/'+applicationId, {failOnStatusCode: false})
 
         cy.contains('E.1 - Project lump sums').click();
         cy.contains('button', 'Add').click();
