af1f17bd62144ee5f156c43ec35a4ffdca3bceef
Ondrej Oravčok <Ondrej.Oravcok@cloudflight.io>
2022-09-28 23:23:59 +0200

    MP2-2755 fix for sometimes missing partners in specific sections

--- jems-server/src/main/kotlin/io/cloudflight/jems/server/project/repository/partner/ProjectPartnerRepository.kt
+++ jems-server/src/main/kotlin/io/cloudflight/jems/server/project/repository/partner/ProjectPartnerRepository.kt
@@ -286,8 +286,9 @@ interface ProjectPartnerRepository : JpaRepository<ProjectPartnerEntity, Long> {
              entity.sort_number as sortNumber,
              addresses.country
              FROM #{#entityName} FOR SYSTEM_TIME AS OF TIMESTAMP :timestamp AS entity
-             LEFT JOIN #{#entityName}_address FOR SYSTEM_TIME AS OF TIMESTAMP :timestamp AS addresses ON entity.id = addresses.partner_id
-             WHERE entity.project_id = :projectId AND (addresses.type = 'Organization' || addresses.type IS NULL)
+               LEFT JOIN #{#entityName}_address FOR SYSTEM_TIME AS OF TIMESTAMP :timestamp AS addresses
+                 ON entity.id = addresses.partner_id AND addresses.type = 'Organization'
+             WHERE entity.project_id = :projectId
              """,
         countQuery = """
              SELECT
@@ -303,20 +304,18 @@ interface ProjectPartnerRepository : JpaRepository<ProjectPartnerEntity, Long> {
         timestamp: Timestamp
     ): Page<PartnerSimpleRow>
 
-    @Query(
-        """
-             SELECT
-             entity.*,
-             entity.sort_number as sortNumber,
-             addresses.*
-             FROM #{#entityName} FOR SYSTEM_TIME AS OF TIMESTAMP :timestamp AS entity
-             LEFT JOIN #{#entityName}_address FOR SYSTEM_TIME AS OF TIMESTAMP :timestamp AS addresses ON entity.id = addresses.partner_id
-             WHERE entity.project_id = :projectId AND (addresses.type = 'Organization' || addresses.type IS NULL)
-             ORDER BY entity.sort_number ASC
-             LIMIT 30
-             """,
-        nativeQuery = true
-    )
+    @Query("""
+        SELECT
+          entity.*,
+          entity.sort_number as sortNumber,
+          addresses.*
+        FROM #{#entityName} FOR SYSTEM_TIME AS OF TIMESTAMP :timestamp AS entity
+            LEFT JOIN #{#entityName}_address FOR SYSTEM_TIME AS OF TIMESTAMP :timestamp AS addresses
+                ON entity.id = addresses.partner_id AND addresses.type = 'Organization'
+        WHERE entity.project_id = :projectId
+        ORDER BY entity.sort_number ASC
+        LIMIT 30
+    """, nativeQuery = true)
     fun findTop30ByProjectIdSortBySortNumberAsOfTimestamp(
         projectId: Long,
         timestamp: Timestamp
