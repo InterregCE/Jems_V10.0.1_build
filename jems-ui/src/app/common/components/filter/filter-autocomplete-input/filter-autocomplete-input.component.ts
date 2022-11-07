455589c255554a53e9982a21760905a724997a49
CATALYSTS\uacar <utku.acar@cloudflight.io>
2022-11-07 16:28:21 +0300

    MP2-2875 Autocomplete filter chip alignment disorder fixed

--- jems-ui/src/app/common/components/filter/filter-autocomplete-input/filter-autocomplete-input.component.ts
+++ jems-ui/src/app/common/components/filter/filter-autocomplete-input/filter-autocomplete-input.component.ts
@@ -26,7 +26,7 @@ export class FilterAutocompleteInputComponent extends FilterListInputComponent {
   @Input()
   hasPartialLockingOfValues: boolean;
   @Input()
-  tooltipContent: String = '';
+  infoContent: String = '';
 
   disabled = false;
 
