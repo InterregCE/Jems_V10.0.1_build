5d5e3672618cd41483b93395dc5101fdba947c8c
Tomasz Buga <tomasz.buga@cloudflight.io>
2023-03-17 09:15:21 +0100

    TB-745: finished polishing (fixed bugs, added missing data and wrapped some things with cy.wrap method)

--- jems-ui/e2e/cypress/e2e/project/reporting/partner-page.spec.ts
+++ jems-ui/e2e/cypress/e2e/project/reporting/partner-page.spec.ts
@@ -16,28 +16,38 @@ context('Partner reports tests', () => {
           verifyPartnerChangesBeforeApproving(testData, applicationId, partnerIdsToDisable);
           cy.approveModification(applicationId, testData.approvalInfo, user.programmeUser.email);
 
-          cy.visit(`https://amsterdam.interact-eu.net/app/project/detail/${applicationId}`, {failOnStatusCode: false})
-            .then(() => {
-              partnerIdsToDisable.forEach(id => {
-                cy.get('mat-expansion-panel-header:contains("Partner details")')
-                  .next('div')
-                  .find(`li:contains("${testData.partners[id].abbreviation}")`)
-                  .contains('mat-icon', 'person_off')
-                  .should('exist');
-              });
+          cy.loginByRequest(user.admin.email).then(() => {
+            cy.visit(`https://amsterdam.interact-eu.net/app/project/detail/${applicationId}`, {failOnStatusCode: false})
+              .then(() => {
+                partnerIdsToDisable.forEach(id => {
+                  cy.get('mat-expansion-panel-header:contains("Partner details")')
+                    .next('div')
+                    .find(`li:contains("${testData.partners[id].abbreviation}")`)
+                    .contains('mat-icon', 'person_off')
+                    .should('exist');
+                });
 
-              verifyIconsInProjectPrivileges(testData, partnerIdsToDisable, true);
-            });
+                verifyIconsInProjectPrivileges(testData, partnerIdsToDisable, true);
+              });
+          });
 
           enableModification(applicationId);
-          loginByRequest(user.applicantUser.email);
-          cy.createFullPartner(applicationId, partner);
+          loginByRequest(user.applicantUser.email).then(() => {
+            cy.createFullPartner(applicationId, partner);
+            submitProjectApp(applicationId);
+          });
 
-          cy.loginByRequest(user.admin.email);
-          verifyPartnerAvailability(false);
+          cy.loginByRequest(user.admin.email).then(() => {
+            cy.visit(`https://amsterdam.interact-eu.net/app/project/detail/${applicationId}`, {failOnStatusCode: false})
+            verifyPartnerAvailability(false);
+          });
 
           cy.approveModification(applicationId, testData.approvalInfo, user.programmeUser.email);
-          verifyPartnerAvailability(true);
+
+          cy.loginByRequest(user.admin.email).then(() => {
+            cy.visit(`https://amsterdam.interact-eu.net/app/project/detail/${applicationId}`, {failOnStatusCode: false})
+            verifyPartnerAvailability(true);
+          });
         });
       });
     });
@@ -100,7 +110,6 @@ function disableSelectedPartners(testData, applicationId, partnerIdsToDisable) {
   cy.loginByRequest(user.applicantUser.email);
   cy.visit(`https://amsterdam.interact-eu.net/app/project/detail/${applicationId}/applicationFormPartner`, {failOnStatusCode: false})
     .then(() => {
-      //TODO: add more disabled partners after returning to the previous JSON file
       disablePartnersByIds(testData, partnerIdsToDisable);
       submitProjectApp(applicationId);
     });
@@ -121,6 +130,7 @@ function disablePartnersByIds(testData, partnerIdsToDisable) {
       .click();
 
     cy.contains('div', `Partner "${testData.partners[id].abbreviation}" deactivated successfully`)
+      .scrollIntoView()
       .should('be.visible');
 
     cy.contains('div', `Partner "${testData.partners[id].abbreviation}" deactivated successfully`)
@@ -128,21 +138,20 @@ function disablePartnersByIds(testData, partnerIdsToDisable) {
 
     cy.contains('mat-row', testData.partners[id].abbreviation)
       .contains('button', 'Deactivate partner')
+      .scrollIntoView()
       .should('be.disabled');
 
     cy.contains('mat-row', testData.partners[id].abbreviation)
       .contains('Inactive')
+      .scrollIntoView()
       .should('be.visible');
 
     cy.contains('mat-row', testData.partners[id].abbreviation)
       .contains('mat-icon', 'person_off')
+      .scrollIntoView()
       .should('be.visible');
 
-    cy.contains('mat-expansion-panel-header', 'Project partners')
-      .next('div')
-      .find(`li:contains("${testData.partners[id].abbreviation}")`)
-      .contains('mat-icon', 'person_off')
-      .should('be.visible');
+    verifyIconsInProjectPartners(testData, id, true);
   })
 }
 
@@ -151,27 +160,30 @@ function verifyPartnerChangesBeforeApproving(testData, applicationId, partnerIds
 
   cy.loginByRequest(user.admin.email);
 
-  cy.visit(`https://amsterdam.interact-eu.net/app/project/detail/${applicationId}/applicationFormPartner`, {failOnStatusCode: false}).then(() => {
-    cy.get('mat-sidenav')
-      .should('be.visible');
-
-    partnerIdsToDisable.forEach(id => {
-      cy.contains('mat-row', testData.partners[id].abbreviation)
-        .contains('Inactive')
+  cy.visit(`https://amsterdam.interact-eu.net/app/project/detail/${applicationId}/applicationFormPartner`, {failOnStatusCode: false})
+    .then(() => {
+      cy.get('mat-sidenav')
         .should('be.visible');
 
-      cy.contains('mat-row', testData.partners[id].abbreviation)
-        .contains('mat-icon', 'person_off')
-        .should('be.visible');
+      partnerIdsToDisable.forEach(id => {
+        cy.contains('mat-row', testData.partners[id].abbreviation)
+          .contains('Inactive')
+          .scrollIntoView()
+          .should('be.visible');
 
-      verifyIconsInPartnerDetails(testData, id, false);
-      verifyIconsInProjectPartners(testData, id, true);
+        cy.contains('mat-row', testData.partners[id].abbreviation)
+          .contains('mat-icon', 'person_off')
+          .scrollIntoView()
+          .should('be.visible');
+
+        verifyIconsInPartnerDetails(testData, id, false);
+        verifyIconsInProjectPartners(testData, id, true);
+      });
+
+      verifyIconsInProjectPrivileges(testData, partnerIdsToDisable, false)
+      verifyDeactivatedPartnerBannerDisplay("Partner details", disabledPartnerAbbreviation, false);
+      verifyDeactivatedPartnerBannerDisplay("Project partners", disabledPartnerAbbreviation, true);
     });
-
-    verifyIconsInProjectPrivileges(testData, partnerIdsToDisable, false)
-    verifyDeactivatedPartnerBannerDisplay("Partner details", disabledPartnerAbbreviation, false);
-    verifyDeactivatedPartnerBannerDisplay("Project partners", disabledPartnerAbbreviation, true);
-  });
 }
 
 function verifyIconsInProjectPrivileges(testData, partnerIdsToDisable, shouldIconsBeDisplayed) {
@@ -195,8 +207,18 @@ function verifyIconsInPartnerDetails(testData, id, shouldIconsBeDisplayed) {
   cy.get('mat-expansion-panel-header:contains("Partner details")')
     .next('div')
     .find(`li:contains("${testData.partners[id].abbreviation}")`)
-    .contains('mat-icon', 'person_off')
-    .should(displayFlag);
+    .then((foundElement) => {
+      if (shouldIconsBeDisplayed) {
+        cy.wrap(foundElement)
+          .contains('mat-icon', 'person_off')
+          .scrollIntoView()
+          .should(displayFlag)
+      } else {
+        cy.wrap(foundElement)
+          .contains('mat-icon', 'person_off')
+          .should(displayFlag)
+      }
+    })
 }
 
 function verifyIconsInProjectPartners(testData, id, shouldIconsBeDisplayed) {
@@ -206,8 +228,16 @@ function verifyIconsInProjectPartners(testData, id, shouldIconsBeDisplayed) {
     .next('div')
     .find(`li:contains("${testData.partners[id].abbreviation}")`)
     .contains('mat-icon', 'person_off')
-    .scrollIntoView()
-    .should(displayFlag);
+    .then((foundElement) => {
+      if (shouldIconsBeDisplayed) {
+        cy.wrap(foundElement)
+          .scrollIntoView()
+          .should(displayFlag);
+      } else {
+        cy.wrap(foundElement)
+          .should(displayFlag);
+      }
+    })
 }
 
 function verifyDeactivatedPartnerBannerDisplay(headerTitle, disabledPartnerAbbreviation, shouldBannerBeDisplayed) {
@@ -228,8 +258,18 @@ function verifyPartnerInPartnerDetails(shouldPartnerBeDisplayed) {
 
   cy.get('mat-expansion-panel-header:contains("Partner details")')
     .next('div')
-    .find(`li:contains("${partner.details.abbreviation}")`)
-    .should(displayFlag);
+    .then((foundElement) => {
+      if (shouldPartnerBeDisplayed) {
+        cy.wrap(foundElement)
+          .find(`li:contains("PP30")`)
+          .scrollIntoView()
+          .should(displayFlag)
+      } else {
+        cy.wrap(foundElement)
+          .find(`li:contains("PP30")`)
+          .should(displayFlag)
+      }
+    });
 }
 
 function verifyPartnerAvailabilityInProjectPrivileges(shouldPartnerBeDisplayed) {
@@ -237,9 +277,17 @@ function verifyPartnerAvailabilityInProjectPrivileges(shouldPartnerBeDisplayed) 
 
   cy.contains('Project privileges')
     .click()
-    .then(() => {
-      cy.get(`mat-expansion-panel-header:contains("${partner.details.abbreviation}")`)
-        .should(displayFlag)
+    .then((foundElement) => {
+      if (shouldPartnerBeDisplayed) {
+        cy.wrap(foundElement)
+          .get(`mat-expansion-panel-header:contains("PP30")`)
+          .scrollIntoView()
+          .should(displayFlag)
+      } else {
+        cy.wrap(foundElement)
+          .get(`mat-expansion-panel-header:contains("PP30")`)
+          .should(displayFlag)
+      }
     });
 }
 
