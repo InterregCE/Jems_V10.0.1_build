0d3f4cdd970b5f77c182503f696a1666ccf7ae01
Ondrej Oravčok <Ondrej.Oravcok@cloudflight.io>
2023-03-23 13:26:39 +0100

    MP2-3041 just try to clean multiple subscriptions

--- jems-ui/src/app/project/project-page-template/project-page-template.component.ts
+++ jems-ui/src/app/project/project-page-template/project-page-template.component.ts
@@ -8,9 +8,6 @@ import {ProjectPageTemplateStore} from './project-page-template-store.service';
 import {UntilDestroy, untilDestroyed} from '@ngneat/until-destroy';
 import PermissionsEnum = UserRoleDTO.PermissionsEnum;
 import ProjectStatusEnum = ProjectUserDTO.ProjectStatusEnum;
-import {
-  ProjectApplicationFormVisibilityService
-} from '@project/project-application/containers/project-application-form-page/services/project-application-form-visibility.service';
 import {LocaleDatePipe} from '@common/pipe/locale-date.pipe';
 import {CustomTranslatePipe} from '@common/pipe/custom-translate-pipe';
 
@@ -41,13 +38,6 @@ export class ProjectPageTemplateComponent implements AfterViewInit {
   @Input() descriptionText: string;
   @Input() descriptionKey: string;
 
-  versionWarnData$: Observable<{
-    selected: ProjectVersionDTO | undefined;
-    current: ProjectVersionDTO | undefined;
-    selectedIsCurrent: boolean;
-    versions: ProjectVersionDTO[];
-  }>;
-
   versionSelectData$: Observable<{
     versions: {
       currentVersion: ProjectVersionDTO;
@@ -55,27 +45,42 @@ export class ProjectPageTemplateComponent implements AfterViewInit {
       pastVersions: ProjectVersionDTO[];
     };
     selectedVersion: ProjectVersionDTO | undefined;
+    versionWarnData: {
+      selected: ProjectVersionDTO | undefined;
+      current: ProjectVersionDTO | undefined;
+      selectedIsCurrent: boolean;
+      versions: ProjectVersionDTO[];
+    };
+    isSelectedSameAsLastApproved: boolean;
+    stringForSelectedVersion: string;
   }>;
 
-  constructor(public projectSidenavService: ProjectApplicationFormSidenavService,
-              public pageStore: ProjectPageTemplateStore,
-              private localelDatePipe: LocaleDatePipe,
-              private customTranslatePipe: CustomTranslatePipe,
-              private projectApplicationFormVisibilityService: ProjectApplicationFormVisibilityService) {
-    this.versionWarnData$ = combineLatest([
+  constructor(
+    public projectSidenavService: ProjectApplicationFormSidenavService,
+    public pageStore: ProjectPageTemplateStore,
+    private localelDatePipe: LocaleDatePipe,
+    private customTranslatePipe: CustomTranslatePipe,
+  ) {
+    this.versionSelectData$ = combineLatest([
       this.pageStore.selectedVersion$,
       this.pageStore.currentVersion$,
       this.pageStore.isSelectedVersionCurrent$,
-      this.pageStore.versions$
-    ]).pipe(
-      map(([selectedVersion, currentVersion, selectedIsCurrent, versions]) => ({selected: selectedVersion, current: currentVersion, selectedIsCurrent, versions}))
-    );
-
-    this.versionSelectData$ = combineLatest([
+      this.pageStore.versions$,
       this.versions(),
-      this.pageStore.selectedVersion$
+      this.pageStore.projectStatus$,
     ]).pipe(
-      map(([versions, selectedVersion]) => ({versions, selectedVersion})),
+      map(([selectedVersion, currentVersion, selectedIsCurrent, versions, versions2, projectStatus]) => ({
+        versions: versions2,
+        selectedVersion,
+        versionWarnData: {
+          selected: selectedVersion,
+          current: currentVersion,
+          selectedIsCurrent,
+          versions,
+        },
+        isSelectedSameAsLastApproved: this.isSelectedSameAsLastApproved(selectedVersion),
+        stringForSelectedVersion: selectedVersion ? this.getStatusStringForSelectedVersion(projectStatus, selectedVersion) : '',
+      })),
     );
   }
 
@@ -102,7 +107,7 @@ export class ProjectPageTemplateComponent implements AfterViewInit {
     return currentVersion.status === ProjectStatusDTO.StatusEnum.APPROVED || currentVersion.status === ProjectStatusDTO.StatusEnum.CONTRACTED;
   }
 
-  isSelectedSameAsLastApproved(selectedVersion: ProjectVersionDTO): boolean {
+  private isSelectedSameAsLastApproved(selectedVersion: ProjectVersionDTO | undefined): boolean {
     let isLastApproved = false;
     this.versions().subscribe( versions => {
       isLastApproved = versions.lastApprovedVersion === selectedVersion;
@@ -156,64 +161,50 @@ export class ProjectPageTemplateComponent implements AfterViewInit {
     return currentlyInModification;
   }
 
-  getStatusStringForSelectedVersion(selectedVersion: ProjectVersionDTO): string {
+  private getStatusStringForSelectedVersion(projectStatus: ProjectStatusDTO, selectedVersion: ProjectVersionDTO): string {
     let date = '';
-    let explainDate = true;
-    let isModification = false;
+    let statusString: string = this.customTranslatePipe.transform('common.label.projectapplicationstatus.'+selectedVersion.status);
 
-    this.pageStore.projectStatus$.subscribe(projectStatus => {
-        switch (selectedVersion.status) {
-          case ProjectStatusEnum.APPROVED:
-            if (projectStatus.entryIntoForceDate) { // project was in modification
-              date = this.localelDatePipe.transform(projectStatus.entryIntoForceDate, 'L');
-              isModification = true;
-            }
-            else {  // project has been initially set to approved
-              date = this.localelDatePipe.transform(projectStatus.decisionDate, 'L');
-            }
-            break;
-          case ProjectStatusEnum.MODIFICATIONREJECTED:
-          case ProjectStatusEnum.CONTRACTED:
-              if (projectStatus.entryIntoForceDate) { // project was in modification
-                date = this.localelDatePipe.transform(projectStatus.entryIntoForceDate, 'L');
-                isModification = true;
-              }
-              else {  // project has been initially set to contracted
-                date = this.localelDatePipe.transform(selectedVersion.createdAt, 'L');
-              }
-              break;
-          case ProjectStatusEnum.SUBMITTED:
-          case ProjectStatusEnum.STEP1SUBMITTED:
-          case ProjectStatusEnum.CONDITIONSSUBMITTED:
-          case ProjectStatusEnum.MODIFICATIONSUBMITTED:
-          case ProjectStatusEnum.MODIFICATIONPRECONTRACTINGSUBMITTED:
-                date = this.localelDatePipe.transform(selectedVersion.createdAt, 'L');
-                explainDate = false;
-            break;
-          case ProjectStatusEnum.APPROVEDWITHCONDITIONS:
-          case ProjectStatusEnum.NOTAPPROVED:
-          case ProjectStatusEnum.INELIGIBLE:
-          case ProjectStatusEnum.ELIGIBLE:
-          case ProjectStatusEnum.STEP1APPROVED:
-          case ProjectStatusEnum.STEP1APPROVEDWITHCONDITIONS:
-          case ProjectStatusEnum.STEP1NOTAPPROVED:
-          case ProjectStatusEnum.STEP1INELIGIBLE:
-          case ProjectStatusEnum.STEP1ELIGIBLE:
-                date = this.localelDatePipe.transform(projectStatus.decisionDate, 'L');
-            break;
-          default: // show no date on editable states or while waiting for a decision
-            break;
+    switch (selectedVersion.status) {
+      case ProjectStatusEnum.APPROVED:
+      case ProjectStatusEnum.MODIFICATIONREJECTED:
+      case ProjectStatusEnum.CONTRACTED:
+        if (projectStatus.entryIntoForceDate) { // project was in modification
+          date = this.localelDatePipe.transform(projectStatus.entryIntoForceDate, 'L');
+          statusString += ', ' + this.customTranslatePipe.transform('project.versions.warn.modification');
         }
-    });
-
-    const statusString: string = this.customTranslatePipe.transform('common.label.projectapplicationstatus.'+selectedVersion.status);
-    if (explainDate && date != '') {
-      return isModification ?
-        statusString + ', ' + this.customTranslatePipe.transform('project.versions.warn.modification') + ' ' + date :
-        statusString + ', ' + this.customTranslatePipe.transform('project.versions.warn.decision') + ' ' + date;
+        else if (projectStatus.decisionDate) {  // project has been initially set to approved
+          date = this.localelDatePipe.transform(projectStatus.decisionDate, 'L');
+          statusString += ', ' + this.customTranslatePipe.transform('project.versions.warn.decision');
+        }
+        else {  // project has been initially set to contracted
+          date = this.localelDatePipe.transform(selectedVersion.createdAt, 'L');
+        }
+        break;
+      case ProjectStatusEnum.SUBMITTED:
+      case ProjectStatusEnum.STEP1SUBMITTED:
+      case ProjectStatusEnum.CONDITIONSSUBMITTED:
+      case ProjectStatusEnum.MODIFICATIONSUBMITTED:
+      case ProjectStatusEnum.MODIFICATIONPRECONTRACTINGSUBMITTED:
+        date = this.localelDatePipe.transform(selectedVersion.createdAt, 'L');
+        break;
+      case ProjectStatusEnum.APPROVEDWITHCONDITIONS:
+      case ProjectStatusEnum.NOTAPPROVED:
+      case ProjectStatusEnum.INELIGIBLE:
+      case ProjectStatusEnum.ELIGIBLE:
+      case ProjectStatusEnum.STEP1APPROVED:
+      case ProjectStatusEnum.STEP1APPROVEDWITHCONDITIONS:
+      case ProjectStatusEnum.STEP1NOTAPPROVED:
+      case ProjectStatusEnum.STEP1INELIGIBLE:
+      case ProjectStatusEnum.STEP1ELIGIBLE:
+        date = this.localelDatePipe.transform(projectStatus.decisionDate, 'L');
+        statusString += ', ' + this.customTranslatePipe.transform('project.versions.warn.decision');
+        break;
+      default: // show no date on editable states or while waiting for a decision
+        break;
     }
-    else
-      return statusString + ' ' + date;
+
+    return statusString + ' ' + date;
   }
 
 }
