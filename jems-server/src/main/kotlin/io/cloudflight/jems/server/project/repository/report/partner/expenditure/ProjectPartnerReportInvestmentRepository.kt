e096a58167c12ad01a79c8cb12db08d378d2d12e
vteodorescu <vlad.teodorescu@cloudflight.io>
2023-03-27 12:36:41 +0300

    MP2-3124 - Certificate investments breakdown in project report - fix source of information for creation of project report

--- jems-server/src/main/kotlin/io/cloudflight/jems/server/project/repository/report/partner/expenditure/ProjectPartnerReportInvestmentRepository.kt
+++ jems-server/src/main/kotlin/io/cloudflight/jems/server/project/repository/report/partner/expenditure/ProjectPartnerReportInvestmentRepository.kt
@@ -30,7 +30,7 @@ interface ProjectPartnerReportInvestmentRepository :
     @Query("""
         SELECT new kotlin.Pair(
             investment.investmentId,
-            COALESCE(SUM(investment.current), 0)
+            COALESCE(SUM(investment.totalEligibleAfterControl), 0)
         )
         FROM #{#entityName} investment
         WHERE investment.reportEntity.id IN :reportIds
