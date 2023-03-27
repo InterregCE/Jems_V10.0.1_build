ac81e93b54cbb3066f7565318fcbe111175b5563
Ondrej Oravčok <Ondrej.Oravcok@cloudflight.io>
2023-03-27 23:25:14 +0200

    MP2-3089 fix table behavior and default sorting

--- jems-ui/src/app/common/components/notification-list/notification-list-store.service.ts
+++ jems-ui/src/app/common/components/notification-list/notification-list-store.service.ts
@@ -1,15 +1,16 @@
 import {Injectable} from '@angular/core';
 import {combineLatest, Observable, Subject} from 'rxjs';
-import {CallService, NotificationService, PageCallDTO, PageNotificationDTO, UserRoleDTO} from '@cat/api';
+import {NotificationService, PageNotificationDTO} from '@cat/api';
 import {MatSort} from '@angular/material/sort';
 import {PermissionService} from '../../../security/permissions/permission.service';
 import {map, startWith, switchMap, tap} from 'rxjs/operators';
 import {Tables} from '@common/utils/tables';
 import {Log} from '@common/utils/log';
-import PermissionsEnum = UserRoleDTO.PermissionsEnum;
 
 @Injectable()
 export class NotificationListStoreService {
+
+  static DEFAULT_SORT: Partial<MatSort> = {active: 'created', direction: 'desc'};
 
   notificationPage$: Observable<PageNotificationDTO>;
   newPageSize$ = new Subject<number>();
@@ -26,8 +27,8 @@ export class NotificationListStoreService {
       this.newPageIndex$.pipe(startWith(Tables.DEFAULT_INITIAL_PAGE_INDEX)),
       this.newPageSize$.pipe(startWith(Tables.DEFAULT_INITIAL_PAGE_SIZE)),
       this.newSort$.pipe(
-        startWith(Tables.DEFAULT_INITIAL_SORT),
-        map(sort => sort?.direction ? sort : Tables.DEFAULT_INITIAL_SORT),
+        startWith(NotificationListStoreService.DEFAULT_SORT),
+        map(sort => sort?.direction ? sort : NotificationListStoreService.DEFAULT_SORT),
         map(sort => `${sort.active},${sort.direction}`)
       )
     ])
