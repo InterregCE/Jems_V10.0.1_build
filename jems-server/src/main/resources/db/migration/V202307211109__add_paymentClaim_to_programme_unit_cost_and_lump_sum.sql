3e7cafc797613e93786f235f86d0856724e3591a
Markus Wizany <markus.wizany@cloudflight.io>
2023-07-24 08:58:55 +0000

    MP2-3482 Flag SCO in programme setup

--- jems-server/src/main/resources/db/migration/V202307211109__add_paymentClaim_to_programme_unit_cost_and_lump_sum.sql
+++ jems-server/src/main/resources/db/migration/V202307211109__add_paymentClaim_to_programme_unit_cost_and_lump_sum.sql
@@ -1,3 +1,5 @@
+SET @@system_versioning_alter_history = 1;
+
 ALTER TABLE programme_unit_cost
     ADD COLUMN payment_claim varchar(50) NOT NULL DEFAULT 'IncurredByBeneficiaries' AFTER foreign_currency_code;
 ALTER TABLE programme_lump_sum
