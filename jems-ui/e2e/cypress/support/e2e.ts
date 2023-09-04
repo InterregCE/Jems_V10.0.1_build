df6a08508dec4c0422f8c09cdc2d5aa6588c58c4
Jovan Ilić <jovan.ilic@cloudflight.io>
2023-09-04 13:26:32 +0000

    Cleanup, bump dependencies

--- jems-ui/e2e/cypress/support/e2e.ts
+++ jems-ui/e2e/cypress/support/e2e.ts
@@ -6,10 +6,3 @@ import './programme.commands';
 import './system.commands';
 import './partner-report.commands';
 import './controller.commands';
-
-// update test case execution results in Jira
-afterEach(function () {
-  if (Cypress.env('executionKey')) {
-    cy.task('sendTestResults', {state: this.currentTest.state, title: this.currentTest.title});
-  }
-});
