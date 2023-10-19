53fe5268ea672c7e260f4b0c9dd1881240214b8c
Botond Lovasz <botond.lovasz@cloudflight.io>
2023-10-19 09:47:02 +0300

    [MP2-3959] - Extend Audit/Control
    - Fix enum GOA

--- jems-server/src/main/kotlin/io/cloudflight/jems/server/project/service/auditAndControl/model/ControllingBody.kt
+++ jems-server/src/main/kotlin/io/cloudflight/jems/server/project/service/auditAndControl/model/ControllingBody.kt
@@ -1,14 +1,13 @@
 package io.cloudflight.jems.server.project.service.auditAndControl.model
 
 enum class ControllingBody(val key: String) {
-
     Controller("Controller"),
     NationalApprobationBody("NationalApprobationBody"),
     RegionalApprobationBody("RegionalApprobationBody"),
     JS("JS"),
     MA("MA"),
     NA("NA"),
-    GOA("GoA"),
+    GoA("GoA"),
     AA("AA"),
     EC("EC"),
     ECA("ECA"),
