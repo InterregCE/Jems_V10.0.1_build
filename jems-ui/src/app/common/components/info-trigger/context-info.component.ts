e33b4eba02754c6fda83f9596022b648ceba018d
Botond Lovasz <botond.lovasz@cloudflight.io>
2024-01-08 13:32:24 +0000

    [MP2-4140][External-NWE] - InfoBubbles

--- jems-ui/src/app/common/components/info-trigger/context-info.component.ts
+++ jems-ui/src/app/common/components/info-trigger/context-info.component.ts
@@ -1,5 +1,7 @@
-import {ChangeDetectionStrategy, Component, Input} from '@angular/core';
+import {ChangeDetectionStrategy, Component, Input, OnInit} from '@angular/core';
 import {TooltipPosition} from '@angular/material/tooltip';
+import {TranslateService} from "@ngx-translate/core";
+import {LanguageStore} from "@common/services/language-store.service";
 
 @Component({
   selector: 'jems-context-info',
@@ -7,10 +9,16 @@ import {TooltipPosition} from '@angular/material/tooltip';
   styleUrls: ['./context-info.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ContextInfoComponent {
+export class ContextInfoComponent implements OnInit{
 
   @Input()
   infoText?: string;
+
+  @Input()
+  icon?: string = 'info_outline';
+
+  @Input()
+  link?: string;
 
   /*
     InfoPosition
@@ -23,5 +31,42 @@ export class ContextInfoComponent {
 
   @Input()
   noWidth: Boolean = false;
+
+  isDisplayed = true;
+
+  isLinkActive = false;
+  // CSS Class name to enable newline character (\n) in ToolTip component
+  newLineClass = 'newline';
+
+  constructor(private translateService: TranslateService, private languageStore: LanguageStore) { }
+
+  ngOnInit(): void {
+    if(!this.infoText) {
+      this.isDisplayed = false;
+      return;
+    }
+
+    if(this.link) {
+        this.infoText = this.infoText+'\n'
+          + this.translateService.instant('common.infobubble.follow.link.text')+'\n'
+          + this.trimLink(this.link,64);
+        this.isLinkActive = true;
+      } else {
+        this.isLinkActive = false;
+      }
+  }
+
+  openLink() {
+    if (this.isLinkActive) {
+      window.open(this.link, '_blank');
+    }
+  }
+
+  trimLink(link: string, size: number): string {
+    if (link.length <= size ){
+      return link;
+    }
+    return link.slice(0, size) + '...';
+  }
 
 }
