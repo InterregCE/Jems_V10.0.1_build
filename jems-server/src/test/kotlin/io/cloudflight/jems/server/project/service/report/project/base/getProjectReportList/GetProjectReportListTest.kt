cc1b0dd188cff9a965d8efded2242b2df27af305
Ondrej Oravčok <Ondrej.Oravcok@cloudflight.io>
2024-01-10 10:08:17 +0100

    MP2-4145 hide values that cant be filled in or does not make sense - fix translation - fix amount requested when draft

--- jems-server/src/test/kotlin/io/cloudflight/jems/server/project/service/report/project/base/getProjectReportList/GetProjectReportListTest.kt
+++ jems-server/src/test/kotlin/io/cloudflight/jems/server/project/service/report/project/base/getProjectReportList/GetProjectReportListTest.kt
@@ -2,6 +2,7 @@ package io.cloudflight.jems.server.project.service.report.project.base.getProjec
 
 import io.cloudflight.jems.server.UnitTest
 import io.cloudflight.jems.server.project.service.ProjectPersistence
+import io.cloudflight.jems.server.project.service.contracting.model.reporting.ContractingDeadlineType
 import io.cloudflight.jems.server.project.service.report.model.project.ProjectReportStatus
 import io.cloudflight.jems.server.project.service.report.partner.financialOverview.ProjectPartnerReportExpenditureCoFinancingPersistence
 import io.cloudflight.jems.server.project.service.report.project.base.ProjectReportPersistence
@@ -41,6 +42,7 @@ internal class GetProjectReportListTest : UnitTest() {
         every { projectPersistence.getProjectPeriods(any(), "v4") } returns listOf(period7)
         every { projectPersistence.getProjectPeriods(any(), "v5") } returns listOf(period8)
         every { certificateCoFinancingPersistence.getTotalsForProjectReports(any())} returns mapOf(
+            5L to BigDecimal.valueOf(4L),
             7L to BigDecimal.ONE,
         )
         every { reportSpfClaimPersistence.getCurrentSpfContributions(any())} returns mapOf(
@@ -56,7 +58,7 @@ internal class GetProjectReportListTest : UnitTest() {
             listOf(
                 report.copy(id = 5L, periodNumber = 5, totalEligibleAfterVerification = null),
                 report.copy(id = 6L, periodNumber = 6, totalEligibleAfterVerification = null),
-                report.copy(id = 7L, periodNumber = 7),
+                report.copy(id = 7L, periodNumber = 7, type = ContractingDeadlineType.Finance),
                 report.copy(id = 8L, periodNumber = 8, status = ProjectReportStatus.InVerification,
                     linkedFormVersion = "v5", totalEligibleAfterVerification = BigDecimal.ZERO),
                 report.copy(id = 9L, periodNumber = 9, status = ProjectReportStatus.Finalized,
@@ -70,9 +72,10 @@ internal class GetProjectReportListTest : UnitTest() {
 
         assertThat(interactor.findAll(projectId, Pageable.unpaged())).containsExactly(
             expectedReportSummary.copy(id = 5L, deletable = true, periodDetail = null,
-                totalEligibleAfterVerification = null, amountRequested = BigDecimal.TEN),
+                totalEligibleAfterVerification = null, amountRequested = BigDecimal.valueOf(14L)),
             expectedReportSummary.copy(id = 6L, deletable = true, totalEligibleAfterVerification = null, periodDetail = null),
-            expectedReportSummary.copy(id = 7L, deletable = true, amountRequested = BigDecimal.valueOf(4L), periodDetail = period7),
+            expectedReportSummary.copy(id = 7L, deletable = true, type = ContractingDeadlineType.Finance,
+                amountRequested = BigDecimal.valueOf(4L), periodDetail = period7),
             expectedReportSummary.copy(id = 8L, status = ProjectReportStatus.InVerification,
                 totalEligibleAfterVerification = null, linkedFormVersion = "v5", periodDetail = period8),
             expectedReportSummary.copy(id = 9L, status = ProjectReportStatus.Finalized, periodDetail = null,
--- jems-ui/src/app/project/project-application/report/project-report/project-report-detail-page/project-report-financial-overview-tab/project-breakdown-per-partner/project-breakdown-per-partner.component.ts
+++ jems-ui/src/app/project/project-application/report/project-report/project-report-detail-page/project-report-financial-overview-tab/project-breakdown-per-partner/project-breakdown-per-partner.component.ts
@@ -43,7 +43,6 @@ export class ProjectBreakdownPerPartnerComponent implements OnChanges {
       ... this.allowedCostCategories.get(CategoryEnum.Multiple) ? ['otherCosts'] : [],
       ... this.allowedCostCategories.get('LumpSum') ? ['lumpSum'] : [],
       ... this.allowedCostCategories.get('UnitCost') ? ['unitCosts'] : [],
-      ... this.allowedCostCategories.get(CategoryEnum.SpfCosts) ? ['spfCosts'] : [],
       'totalBudget'
     ];
   }
