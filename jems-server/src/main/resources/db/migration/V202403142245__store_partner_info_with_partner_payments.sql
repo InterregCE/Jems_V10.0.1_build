6b9346280911c1d1a68c937e98a71597e137f428
Ondrej Oravčok <Ondrej.Oravcok@cloudflight.io>
2024-03-19 10:00:02 +0100

    MP2-4136 fix wrong migration

--- jems-server/src/main/resources/db/migration/V202403142245__store_partner_info_with_partner_payments.sql
+++ jems-server/src/main/resources/db/migration/V202403142245__store_partner_info_with_partner_payments.sql
@@ -6,7 +6,7 @@ ALTER TABLE payment_partner
 UPDATE payment_partner
     LEFT JOIN payment ON payment_partner.payment_id = payment.id
     LEFT JOIN project_partner ON payment_partner.partner_id = project_partner.id
-SET partner_abbreviation = project_partner.abbreviation,
-    partner_name_in_original_language = project_partner.name_in_original_language,
-    partner_name_in_english = project_partner.name_in_english
+SET partner_abbreviation = IF(project_partner.abbreviation IS NOT NULL, project_partner.abbreviation, ''),
+    partner_name_in_original_language = IF(project_partner.name_in_original_language IS NOT NULL, project_partner.name_in_original_language, ''),
+    partner_name_in_english = IF(project_partner.name_in_english IS NOT NULL, project_partner.name_in_english, '')
 WHERE payment.type = 'FTLS';
