463d2e4587fd9980f5d3cb0fdb6bafc2512043ed
Jovan Ilić <jovan.ilic@cloudflight.io>
2024-03-27 11:36:56 +0100

    TB-1154 Rounding values in Project budget overview tables

--- jems-ui/e2e/cypress/e2e/project/application-form/project-budget.spec.ts
+++ jems-ui/e2e/cypress/e2e/project/application-form/project-budget.spec.ts
@@ -17,92 +17,92 @@ context('Project budget tests', () => {
     cy.fixture('project/application-form/project-budget/TB-534.json').then(testData => {
       cy.fixture('api/application/application.json').then(application => {
 
-      cy.loginByRequest(user.programmeUser.email);
-      call.budgetSettings.flatRates = testData.call.flatRates;
-      call.generalCallSettings.additionalFundAllowed = false;
-      cy.createCall(call).then(callId => {
-        application.details.projectCallId = callId;
-        cy.publishCall(callId);
-      });
-
-      cy.loginByRequest(user.applicantUser.email);
-
-      application.partners = [];
-      testData.partners.forEach(partnerData => {
-        const tempPartner = JSON.parse(JSON.stringify(partner));
-        tempPartner.details.abbreviation = partnerData.abbreviation;
-        tempPartner.details.role = partnerData.role;
-        tempPartner.budget = partnerData.budget;
-        tempPartner.cofinancing = partnerData.cofinancing;
-        application.partners.push(tempPartner);
-      });
-      application.associatedOrganisations = null;
-      application.lumpSums = [];
-      application.description.workPlan[0].activities[0].cypressReferencePartner = application.partners[0].details.abbreviation;
-      application.description.workPlan[0].activities[1].cypressReferencePartner = application.partners[1].details.abbreviation;
-
-      cy.createApprovedApplication(application, user.programmeUser.email).then(applicationId => {
-        cy.visit(`app/project/detail/${applicationId}/applicationFormOverviewTables`, {failOnStatusCode: false});
-
-        // A.3
-        cy.contains('tr', 'ERDF').should('contain', testData.fundingAmounts.totalERDF);
-        cy.contains('tr', 'Neighbourhood CBC').should('contain', testData.fundingAmounts.totalCBC);
-        cy.contains('tr', 'Total project budget').should('contain', testData.fundingAmounts.total);
-        cy.contains('tr', 'Total project budget').should('contain', testData.fundingAmounts.automaticPublicContribution);
-        cy.contains('tr', 'Total project budget').should('contain', testData.fundingAmounts.totalPublicContribution);
-        cy.contains('tr', 'Total project budget').should('contain', testData.fundingAmounts.privateContribution);
-
-        // D.1
-        cy.visit(`app/project/detail/${applicationId}/applicationFormBudgetPerPartner`, {failOnStatusCode: false});
-        cy.intercept(`api/project/${applicationId}/workPackage`).as('pageLoaded');
-        cy.wait('@pageLoaded');
-
-        cy.get('div.footer').should('contain', testData.fundingAmounts.totalERDF);
-        cy.get('div.footer').should('contain', testData.fundingAmounts.totalCBC);
-        cy.get('div.footer').should('contain', testData.fundingAmounts.total);
-        cy.get('div.footer').should('contain', testData.fundingAmounts.automaticPublicContribution);
-        cy.get('div.footer').should('contain', testData.fundingAmounts.totalPublicContribution);
-        cy.get('div.footer').should('contain', testData.fundingAmounts.privateContribution);
-
-        // D.2
-        cy.visit(`app/project/detail/${applicationId}/applicationFormBudget`, {failOnStatusCode: false});
-
-        testData.partners.forEach((partner, partnerIndex) => {
-          cy.contains('div', partner.abbreviation).find('span').each((partnerBudget, budgetIndex) => {
-            if (![0, 1, 2].includes(budgetIndex))
-              expect(partnerBudget.text()).to.be.equal(testData.partners[partnerIndex].budgetOverview[budgetIndex - 3]);
-          });
+        cy.loginByRequest(user.programmeUser.email);
+        call.budgetSettings.flatRates = testData.call.flatRates;
+        call.generalCallSettings.additionalFundAllowed = false;
+        cy.createCall(call).then(callId => {
+          application.details.projectCallId = callId;
+          cy.publishCall(callId);
         });
 
-        cy.get('div.footer').find('span').each((totalBudget, budgetIndex) => {
-          if (![0, 1, 2].includes(budgetIndex))
-            expect(totalBudget.text()).to.be.equal(testData.fundingAmounts.totalBudgetOverview[budgetIndex - 3]);
+        cy.loginByRequest(user.applicantUser.email);
+
+        application.partners = [];
+        testData.partners.forEach(partnerData => {
+          const tempPartner = JSON.parse(JSON.stringify(partner));
+          tempPartner.details.abbreviation = partnerData.abbreviation;
+          tempPartner.details.role = partnerData.role;
+          tempPartner.budget = partnerData.budget;
+          tempPartner.cofinancing = partnerData.cofinancing;
+          application.partners.push(tempPartner);
         });
+        application.associatedOrganisations = null;
+        application.lumpSums = [];
+        application.description.workPlan[0].activities[0].cypressReferencePartner = application.partners[0].details.abbreviation;
+        application.description.workPlan[0].activities[1].cypressReferencePartner = application.partners[1].details.abbreviation;
 
-        // D.3
-        cy.visit(`app/project/detail/${applicationId}/applicationFormBudgetPerPeriod`, {failOnStatusCode: false});
+        cy.createApprovedApplication(application, user.programmeUser.email).then(applicationId => {
+          cy.visit(`app/project/detail/${applicationId}/applicationFormOverviewTables`, {failOnStatusCode: false});
 
-        cy.contains('div', 'ERDF').should('contain', testData.fundingAmounts.totalERDF);
-        cy.contains('div', 'Neighbourhood CBC').should('contain', testData.fundingAmounts.totalCBC);
-        cy.get('div.footer').should('contain', testData.fundingAmounts.total);
+          // A.3
+          cy.contains('tr', 'ERDF').should('contain', testData.fundingAmounts.totalERDF);
+          cy.contains('tr', 'Neighbourhood CBC').should('contain', testData.fundingAmounts.totalCBC);
+          cy.contains('tr', 'Total project budget').should('contain', testData.fundingAmounts.total);
+          cy.contains('tr', 'Total project budget').should('contain', testData.fundingAmounts.automaticPublicContribution);
+          cy.contains('tr', 'Total project budget').should('contain', testData.fundingAmounts.totalPublicContribution);
+          cy.contains('tr', 'Total project budget').should('contain', testData.fundingAmounts.privateContribution);
 
-        // Partner budget and co-financing
-        testData.partners.forEach(partner => {
-          cy.get(`@${partner.abbreviation}`).then(partnerId => {
-            cy.visit(`app/project/detail/${applicationId}/applicationFormPartner/${partnerId}/budget`, {failOnStatusCode: false});
+          // D.1
+          cy.visit(`app/project/detail/${applicationId}/applicationFormBudgetPerPartner`, {failOnStatusCode: false});
+          cy.intercept(`api/project/${applicationId}/workPackage`).as('pageLoaded');
+          cy.wait('@pageLoaded');
 
-            cy.get('jems-budget-table div').eq(2).find('span').each((partnerBudget, index) => {
-              if (![0,1].includes(index))
-                expect(partnerBudget.text()).to.be.equal(partner.budgetOverview[index - 2]);
+          cy.get('div.footer').should('contain', testData.fundingAmounts.totalERDF);
+          cy.get('div.footer').should('contain', testData.fundingAmounts.totalCBC);
+          cy.get('div.footer').should('contain', testData.fundingAmounts.total);
+          cy.get('div.footer').should('contain', testData.fundingAmounts.automaticPublicContribution);
+          cy.get('div.footer').should('contain', testData.fundingAmounts.totalPublicContribution);
+          cy.get('div.footer').should('contain', testData.fundingAmounts.privateContribution);
+
+          // D.2
+          cy.visit(`app/project/detail/${applicationId}/applicationFormBudget`, {failOnStatusCode: false});
+
+          testData.partners.forEach((partner, partnerIndex) => {
+            cy.contains('div', partner.abbreviation).find('span').each((partnerBudget, budgetIndex) => {
+              if (![0, 1, 2].includes(budgetIndex))
+                expect(partnerBudget.text()).to.be.equal(testData.partners[partnerIndex].budgetOverview[budgetIndex - 3]);
             });
-            
-            cy.visit(`app/project/detail/${applicationId}/applicationFormPartner/${partnerId}/coFinancing`, {failOnStatusCode: false});
+          });
 
-            cy.contains('div.jems-table-config', 'Source').children().eq(1).find('div').should('contain', partner.cofinancingAmount);
-            cy.contains('div.jems-table-config', 'Source').children().eq(3).find('div').eq(1).should('contain', partner.budgetOverview[9]);
+          cy.get('div.footer').find('span').each((totalBudget, budgetIndex) => {
+            if (![0, 1, 2].includes(budgetIndex))
+              expect(totalBudget.text()).to.be.equal(testData.fundingAmounts.totalBudgetOverview[budgetIndex - 3]);
+          });
+
+          // D.3
+          cy.visit(`app/project/detail/${applicationId}/applicationFormBudgetPerPeriod`, {failOnStatusCode: false});
+
+          cy.contains('div', 'ERDF').should('contain', testData.fundingAmounts.totalERDF);
+          cy.contains('div', 'Neighbourhood CBC').should('contain', testData.fundingAmounts.totalCBC);
+          cy.get('div.footer').should('contain', testData.fundingAmounts.total);
+
+          // Partner budget and co-financing
+          testData.partners.forEach(partner => {
+            cy.get(`@${partner.abbreviation}`).then(partnerId => {
+              cy.visit(`app/project/detail/${applicationId}/applicationFormPartner/${partnerId}/budget`, {failOnStatusCode: false});
+
+              cy.get('jems-budget-table div').eq(2).find('span').each((partnerBudget, index) => {
+                if (![0, 1].includes(index))
+                  expect(partnerBudget.text()).to.be.equal(partner.budgetOverview[index - 2]);
+              });
+
+              cy.visit(`app/project/detail/${applicationId}/applicationFormPartner/${partnerId}/coFinancing`, {failOnStatusCode: false});
+
+              cy.contains('div.jems-table-config', 'Source').children().eq(1).find('div').should('contain', partner.cofinancingAmount);
+              cy.contains('div.jems-table-config', 'Source').children().eq(3).find('div').eq(1).should('contain', partner.budgetOverview[9]);
+            });
           });
         });
-      });
       });
     });
   });
@@ -192,6 +192,41 @@ context('Project budget tests', () => {
                 expect(exportFile.content[1].data.slice(1), assertionMessage).to.deep.equal(testDataFile[1].data.slice(1));
               });
             });
+          });
+        });
+      });
+    });
+  });
+
+
+  it('TB-1154 Rounding values in Project budget overview tables', () => {
+    cy.fixture('api/application/application.json').then(application => {
+      cy.fixture('project/application-form/project-budget/TB-1154.json').then(testData => {
+        cy.loginByRequest(user.applicantUser.email);
+        call.generalCallSettings.funds[1].rate = 100;
+        cy.createCall(call, user.programmeUser.email).then(callId => {
+          application.details.projectCallId = callId;
+          cy.publishCall(callId, user.programmeUser.email);
+
+          const expectedResult = '1.034.921,42'
+
+          // prepare application for rounding calculations
+          application.partners[0].budget = testData.budget;
+          application.partners[0].cofinancing = testData.cofinancingLP;
+          application.partners[1].budget = testData.budget;
+          application.partners[1].cofinancing = testData.cofinancingPP;
+
+          application.lumpSums = [];
+
+          cy.createSubmittedApplication(application).then(applicationId => {
+            cy.visit(`app/project/detail/${applicationId}/applicationFormBudgetPerPartner`, {failOnStatusCode: false});
+
+            // verify Total Neighbourhood CBC amount
+            cy.get('div.jems-table-config div:nth-child(4) span:nth-child(6)').should('contain', expectedResult);
+
+            cy.contains('D.3 Overview budget').click();
+            cy.contains('div', 'Neighbourhood CBC').find('span:nth-child(13)').should('contain', expectedResult);
+            cy.contains('div', 'Total EU Funds').find('span:nth-child(13)').should('contain', expectedResult);
           });
         });
       });
